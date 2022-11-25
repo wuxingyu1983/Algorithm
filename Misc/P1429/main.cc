b548ac7f7ddbe35f2c5d5a439c25c86a251ae573
吴星煜 <wuxingyu@wuxingyudeMacBook-Pro-2.local>
2022-11-26 00:50:21 +0800

    pre process

--- Misc/P1429/main.cc
+++ Misc/P1429/main.cc
@@ -12,6 +12,13 @@ using namespace std;
 
 #define DEBUG   0
 
+bool compare_float(float x, float y, float epsilon = 0.0001f)
+{
+    if (fabs(x - y) < epsilon)
+        return true; // they are same
+    return false;    // they are not same
+}
+
 class Point
 {
 public:
@@ -22,6 +29,35 @@ public:
 };
 
 vector<Point> points;
+
+bool cmp(Point &a, Point &b)
+{
+    bool ret = false;
+
+    if (a.x < b.x)
+    {
+        ret = true;
+    }
+    else if (compare_float(a.x, b.x))
+    {
+        if (a.y < b.y)
+        {
+            ret = true;
+        }
+    }
+    
+
+    return ret;
+}
+
+float distance(Point &a, Point &b)
+{
+    float ret;
+
+    ret = sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+
+    return ret;
+}
 
 int main()
 {
@@ -52,7 +88,46 @@ int main()
     }
 
     // sort
+    sort(points.begin(), points.end(), cmp);
+
+    float min = -1;
+
+    // pre-find min
+    vector<Point>::iterator it = points.begin();
+    vector<Point>::iterator pre = points.end();
+    while (it != points.end())
+    {
+        if (pre == points.end())
+        {
+            pre = it;
+        }
+        else
+        {
+            float tmp = distance(pre, it);
+            if (compare_float(tmp, 0))
+            {
+                min = tmp;
+                break;
+            }
+            else
+            {
+                if (0 > min || tmp < min)
+                {
+                    min = tmp;
+                }
+            }
+        }
+
+        it ++; 
+    }
     
+    if (false == compare_float(min, 0))
+    {
+        // build kd-tree
+
+    }
+
+    cout << fixed << setprecision(4) << min << endl;
 
 #if DEBUG
     inFile.close();

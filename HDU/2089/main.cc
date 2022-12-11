84e2213c7e3ab33024dd17be4a900d94652a41e0
吴星煜 <wuxingyu@wuxingyudeMacBook-Pro.local>
2022-12-11 18:37:45 +0800

    init

---
+++ HDU/2089/main.cc
@@ -0,0 +1,35 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    unsigned int m, n;
+
+    while (true)
+    {
+        cin >> n >> m;
+
+        if (0 == n && 0 == m)
+        {
+            break;
+        }
+
+        unsigned int count = 0;
+
+        
+
+        cout << count << endl;
+    }
+
+    return 0;
+}

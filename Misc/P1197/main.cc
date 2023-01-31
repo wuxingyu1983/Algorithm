c7f859bb94cfe371d25f4683a21047fcf920ead7
吴星煜 <wuxingyu@wuxingyudeMacBook-Pro.local>
2023-01-31 21:13:02 +0800

    TLE

---
+++ Misc/P1197/main.cc
@@ -0,0 +1,202 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <map>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <numeric>
+
+using namespace std;
+
+#define DEBUG 1
+
+class Edge
+{
+public:
+    unsigned int x, y;
+
+    Edge()
+    {
+        x = y = 0;
+    }
+};
+
+bool cmp(Edge &a, Edge &b)
+{
+    bool ret = false;
+
+    if (a.x < b.x)
+    {
+        ret = true;
+    }
+    else if (a.x == b.x)
+    {
+        return a.y <= b.y;
+    }
+
+    return ret;
+}
+
+unsigned int find(vector<unsigned int> &pa, unsigned int x)
+{
+    return pa[x] == x ? x : pa[x] = find(pa, pa[x]);
+}
+
+// set x => set y
+void unite(vector<unsigned int> &pa, unsigned int x, unsigned int y)
+{
+    pa[find(pa, x)] = find(pa, y);
+}
+
+int main()
+{
+#if DEBUG
+    ifstream inFile;
+    inFile.open("input.txt");
+#endif
+
+    int n, m;
+#if DEBUG
+    inFile >> n >> m;
+#else
+    cin >> n >> m;
+#endif
+
+    vector<Edge> edges(m);
+    for (size_t i_m = 0; i_m < m; i_m++)
+    {
+#if DEBUG
+        inFile >> edges[i_m].x >> edges[i_m].y;
+#else
+        cin >> edges[i_m].x >> edges[i_m].y;
+#endif
+        if (edges[i_m].x > edges[i_m].y)
+        {
+            swap(edges[i_m].x, edges[i_m].y);
+        }
+    }
+
+    sort(edges.begin(), edges.end(), cmp);
+
+    unsigned int k;
+#if DEBUG
+    inFile >> k;
+#else
+    cin >> k;
+#endif
+
+    vector<unsigned int> targets(k);
+    set<unsigned int> targets_set;
+    for (size_t i_k = 0; i_k < k; i_k++)
+    {
+#if DEBUG
+        inFile >> targets[i_k];
+#else
+        cin >> targets[i_k];
+#endif
+
+        targets_set.insert(targets[i_k]);
+    }
+
+    // init
+    vector<unsigned int> pa(n);
+    iota(pa.begin(), pa.end(), 0);
+
+    vector<unsigned int> set_size(n, 1);
+
+    multimap<unsigned int, unsigned int> targets_map;
+    unsigned int cnt = n - k;
+    for (size_t i_m = 0; i_m < m; i_m++)
+    {
+        if (targets_set.end() == targets_set.find(edges[i_m].x) && targets_set.end() == targets_set.find(edges[i_m].y))
+        {
+            // x, y not in targets
+            unsigned int pa_x = find(pa, edges[i_m].x);
+            unsigned int pa_y = find(pa, edges[i_m].y);
+
+            if (pa_x != pa_y)
+            {
+                if (set_size[pa_x] < set_size[pa_y])
+                {
+                    unite(pa, pa_x, pa_y);
+                    set_size[pa_y] += set_size[pa_x];
+                }
+                else
+                {
+                    unite(pa, pa_y, pa_x);
+                    set_size[pa_x] += set_size[pa_y];
+                }
+
+                cnt --;
+            }
+        }
+        else
+        {
+            targets_map.insert(make_pair(edges[i_m].x, edges[i_m].y));
+            targets_map.insert(make_pair(edges[i_m].y, edges[i_m].x));
+        }
+    }
+
+    vector<unsigned int> cnts;
+    cnts.push_back(cnt);
+
+    for (vector<unsigned int>::reverse_iterator rit = targets.rbegin(); rit != targets.rend(); rit++)
+    {
+        cnt ++;
+
+        {
+            auto pr = targets_map.equal_range(*rit);
+            if (pr.first != std::end(targets_map))
+            {
+                for (auto iter = pr.first; iter != pr.second; ++iter)
+                {
+                    if (std::end(targets_set) == targets_set.find(iter->second))
+                    {
+                        unsigned int pa_x = find(pa, *rit);
+                        unsigned int pa_y = find(pa, iter->second);
+
+                        if (pa_x != pa_y)
+                        {
+                            if (set_size[pa_x] < set_size[pa_y])
+                            {
+                                unite(pa, pa_x, pa_y);
+                                set_size[pa_y] += set_size[pa_x];
+                            }
+                            else
+                            {
+                                unite(pa, pa_y, pa_x);
+                                set_size[pa_x] += set_size[pa_y];
+                            }
+
+                            cnt--;
+                        }
+                    }
+                }
+            }
+        }
+
+        {
+            auto pr = targets_set.find(*rit);
+            targets_set.erase(pr);
+        }
+
+        cnts.push_back(cnt);
+    }
+
+    for (vector<unsigned int>::reverse_iterator rit = cnts.rbegin(); rit != cnts.rend(); rit++)
+    {
+        cout << *rit << endl;
+    }
+
+#if DEBUG
+    inFile.close();
+#endif
+
+    return 0;
+}

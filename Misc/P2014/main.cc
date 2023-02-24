7b35bea28f70269c92f313abc01cbe437fb80572
吴星煜 <wuxingyu@wuxingyudeMacBook-Pro.local>
2023-02-24 13:31:08 +0800

    Accepted

--- Misc/P2014/main.cc
+++ Misc/P2014/main.cc
@@ -15,8 +15,8 @@
 using namespace std;
 
 #define DEBUG       0
-#define MAX_N       301 
-#define MAX_M       301
+#define MAX_N       301
+#define MAX_M       302
 
 class Subject
 {
@@ -24,15 +24,45 @@ public:
     int credit;
     int prerequisite;
     vector<int> followups;
-    int sum[2][MAX_M];  // 0 - not chose, 1 - chose 
+    int sum[MAX_M];  // 0 - not chose, 1 - chose
 
-    Subject
+    Subject()
     {
         credit = prerequisite = 0;
     }
 };
 
 Subject subs[MAX_N];
+
+void func(Subject * sub, int m)
+{
+    sub->sum[1] = sub->credit;
+
+    for (vector<int>::iterator it = sub->followups.begin(); it != sub->followups.end(); it ++)
+    {
+        func(&subs[*it], m);
+    }
+
+    if (0 == sub->credit)
+    {
+        m += 1;
+    }
+
+    for (vector<int>::iterator it = sub->followups.begin(); it != sub->followups.end(); it++)
+    {
+        for (size_t i_m = m; i_m >= 2; i_m --)
+        {
+            for (size_t j = 1; j <= i_m - 1; j ++)
+            {
+                int tmp = sub->sum[i_m - j] + subs[*it].sum[j];
+                if (tmp > sub->sum[i_m])
+                {
+                    sub->sum[i_m] = tmp;
+                }
+            }
+        }
+    }
+}
 
 int main()
 {
@@ -62,7 +92,9 @@ int main()
         subs[k].followups.push_back(i);
     }
 
-    
+    func(&subs[0], m);
+
+    cout << subs[0].sum[m + 1] << endl;
 
 #if DEBUG
     inFile.close();

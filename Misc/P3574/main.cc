79906f9443cbd0ebddee453f1494d90343fa2a56
吴星煜 <wuxingyu@wuxingyudeMacBook-Pro.local>
2023-04-01 15:19:58 +0800

    80.00 points

--- Misc/P3574/main.cc
+++ Misc/P3574/main.cc
@@ -23,10 +23,13 @@ public:
     unsigned int t;
     unsigned int max;
     int diff;
+    int depth;
+
+    vector<House *> children;
 
     House()
     {
-        diff = t = max = 0;
+        depth = diff = t = max = 0;
     }
 };
 
@@ -35,70 +38,27 @@ inline bool comp(House *a, House *b)
     return (a->diff > b->diff);
 }
 
-vector<House> houses(MAX_N);
-multimap<int, int> edges;
-vector<House *> children(MAX_N);
-
-void init_houses(int curr, int parent)
+inline bool compDepth(House * a, House * b)
 {
+    return (a->depth > b->depth);
+}
+
+vector<House> raws(MAX_N);
+vector<House *> houses(MAX_N);
+multimap<int, int> edges;
+
+void init_tree(int curr, int parent, int depth)
+{
+    houses[curr]->depth = depth;
     pair<multimap<int, int>::iterator, multimap<int, int>::iterator> ret = edges.equal_range(curr);
 
-    int size = 0;
     for (multimap<int, int>::iterator it = ret.first; it != ret.second; ++it)
     {
         if (parent != it->second)
         {
-            init_houses(it->second, curr);
+            init_tree(it->second, curr, depth + 1);
+            houses[curr]->children.push_back(houses[it->second]);
         }
-    }
-
-    for (multimap<int, int>::iterator it = ret.first; it != ret.second; ++it)
-    {
-        if (parent != it->second)
-        {
-            children[size++] = &(houses[it->second]);
-        }
-    }
-
-    if (0 < size)
-    {
-        // sort, desc
-        sort(children.begin(), children.begin() + size, comp);
-
-        int t = 0;
-        int max = 0;
-
-        for (vector<House *>::iterator it = children.begin(); it != children.begin() + size; it++)
-        {
-            if (t + (*it)->max + 1 > max)
-            {
-                max = t + (*it)->max + 1;
-            }
-
-            t += (*it)->t + 2;
-        }
-
-        houses[curr].t = t;
-        if (1 != curr)
-        {
-            if (houses[curr].max < max)
-            {
-                houses[curr].max = max;
-            }
-        }
-        else
-        {
-            if (houses[curr].max + houses[curr].t > max)
-            {
-                houses[curr].max = houses[curr].max + houses[curr].t;
-            }
-            else
-            {
-                houses[curr].max = max;
-            }
-        }
-
-        houses[curr].diff = houses[curr].max - houses[curr].t;
     }
 }
 
@@ -125,7 +85,8 @@ int main()
         cin >> c;
 #endif
 
-        houses[i].max = houses[i].diff = c;
+        raws[i].max = raws[i].diff = c;
+        houses[i] = &(raws[i]);
     }
 
     for (size_t i = 1; i < n; i++)
@@ -140,9 +101,57 @@ int main()
         edges.insert(pair<int, int>(b, a));
     }
 
-    init_houses(1, 0);
+    init_tree(1, 0, 0);
+//    init_houses(1, 0);
 
-    cout << houses[1].max << endl;
+    // sort houses by depth desc
+    sort(houses.begin() + 1, houses.begin() + 1 + n, compDepth);
+
+    for (size_t curr = 1; curr <= n; curr++)
+    {
+        if (0 < houses[curr]->children.size())
+        {
+            // sort, desc
+            sort(houses[curr]->children.begin(), houses[curr]->children.end(), comp);
+
+            int t = 0;
+            int max = 0;
+
+            for (vector<House *>::iterator it = houses[curr]->children.begin(); it != houses[curr]->children.end(); it++)
+            {
+                if (t + (*it)->max + 1 > max)
+                {
+                    max = t + (*it)->max + 1;
+                }
+
+                t += (*it)->t + 2;
+            }
+
+            houses[curr]->t = t;
+            if (n != curr)
+            {
+                if (houses[curr]->max < max)
+                {
+                    houses[curr]->max = max;
+                }
+            }
+            else
+            {
+                if (houses[curr]->max + houses[curr]->t > max)
+                {
+                    houses[curr]->max = houses[curr]->max + houses[curr]->t;
+                }
+                else
+                {
+                    houses[curr]->max = max;
+                }
+            }
+
+            houses[curr]->diff = houses[curr]->max - houses[curr]->t;
+        }
+    }
+
+    cout << houses[n]->max << endl;
 
 #if DEBUG
     inFile.close();

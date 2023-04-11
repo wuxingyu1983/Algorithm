d4dbaec0b9cb51c89c7eabac903daddbf6cc9ffc
吴星煜 <wuxingyu@wuxingyudeMacBook-Pro.local>
2023-04-11 13:34:31 +0800

    70

--- LibreOJ/2546/main.cc
+++ LibreOJ/2546/main.cc
@@ -12,6 +12,7 @@
 #include <iomanip>
 #include <numeric>
 #include <queue>
+#include <stack>
 
 using namespace std;
 
@@ -20,113 +21,25 @@ using namespace std;
 #define MAX_K 101
 #define MOD 1000000007
 
-unsigned long long dp[MAX_N][2][4][MAX_K]; // [index][active][type][num]
-unsigned char active[MAX_N];               // 0, 1
+unsigned int dp[MAX_N][2][4][MAX_K]; // [index][active][type][num]
+unsigned char active[MAX_N];         // 0, 1
 vector< vector<unsigned int> > vertexes(MAX_N);
 unsigned int cnt[MAX_N];
 unsigned int n, k;
+stack<unsigned int> nodes;
+unsigned int parents[MAX_N];
 
-void func(unsigned int node, unsigned int parent)
+void init(unsigned int node, unsigned int parent)
 {
-    unsigned char act = active[node];
-
-    cnt[node] = 1;
-    if (0 < parent && 1 == vertexes[node].size())
-    {
-        // leaf, no children
-        dp[node][act][0][0] = 1;
-        dp[node][act][2][1] = 1;
-    }
-    else
-    {
-        vector<unsigned int>::iterator it = vertexes[node].begin();
-
-        if (parent == *it)
-        {
-            it++;
-        }
-
-        func(*it, node);
-        cnt[node] += cnt[*it];
-
-        // first
-        {
-            unsigned char chact = active[*it];
-
-            unsigned int upper = k;
-            if(k > cnt[node])
-            {
-                upper = cnt[node];
-            }
-            for (size_t num = 0; num <= upper; num++)
-            {
-                // type : 0, no equipment in node, and node is not monitored
-                dp[node][act][0][num] = dp[*it][chact][1][num];
-
-                // type : 1,  no equipment in node, and node is monitored
-                dp[node][act][1][num] = dp[*it][chact][3][num];
-
-                if (0 < num)
-                {
-                    // type : 2, equipment in node, and node is not monitored
-                    dp[node][act][2][num] = dp[*it][chact][0][num - 1] + dp[*it][chact][1][num - 1];
-
-                    // type : 3, equipment in node, and node is monitored
-                    dp[node][act][3][num] = dp[*it][chact][2][num - 1] + dp[*it][chact][3][num - 1];
-                }
-            }
-        }
-
-        for (it++; it != vertexes[node].end(); it++)
+    for (vector<unsigned int>::iterator it = vertexes[node].begin(); it != vertexes[node].end(); it++)
         {
             if (parent != *it)
             {
-                func(*it, node);
-                cnt[node] += cnt[*it];
-
-                act = 1 - act;
-                unsigned char chact = active[*it];
-
-                // clear
-                memset(dp[node][act], 0, 4 * sizeof(unsigned long long) * MAX_K);
-
-                unsigned int upper = k;
-                if (k > cnt[node])
-                {
-                    upper = cnt[node];
-                }
-                for (size_t num = 0; num <= upper; num++)
-                {
-                    for (size_t i = 0; i <= num; i++)
-                    {
-                        // type : 0, no equipment in node, and node is not monitored
-                        dp[node][act][0][num] += dp[node][1 - act][0][i] * dp[*it][chact][1][num - i];
-                        dp[node][act][0][num] %= MOD;
-
-                        // type : 1,  no equipment in node, and node is monitored
-                        dp[node][act][1][num] += dp[node][1 - act][0][i] * dp[*it][chact][3][num - i];
-                        dp[node][act][1][num] += dp[node][1 - act][1][i] * (dp[*it][chact][3][num - i] + dp[*it][chact][1][num - i]);
-                        dp[node][act][1][num] %= MOD;
-
-                        //                        if (0 < num && i < num)
-                        {
-                            // type : 2, equipment in node, and node is not monitored
-                            dp[node][act][2][num] += dp[node][1 - act][2][i] * (dp[*it][chact][0][num - i] + dp[*it][chact][1][num - i]);
-                            dp[node][act][2][num] %= MOD;
-
-                            // type : 3, equipment in node, and node is monitored
-                            dp[node][act][3][num] += dp[node][1 - act][2][i] * (dp[*it][chact][2][num - i] + dp[*it][chact][3][num - i]);
-                            dp[node][act][3][num] += dp[node][1 - act][3][i] * (dp[*it][chact][0][num - i] + dp[*it][chact][1][num - i] +
-                                                                                dp[*it][chact][2][num - i] + dp[*it][chact][3][num - i]);
-                            dp[node][act][3][num] %= MOD;
-                        }
-                    }
-                }
+                nodes.push(*it);
+                init(*it, node);
+                parents[*it] = node;
             }
         }
-
-        active[node] = act;
-    }
 }
 
 int main()
@@ -155,14 +68,145 @@ int main()
         vertexes[v2].push_back(v1);
     }
 
-    unsigned long long ans = 0;
+    nodes.push(1);
+    init(1, 0);
 
-    func(1, 0);
+    while (0 < nodes.size())
+    {
+        unsigned int node = nodes.top();
+
+        {
+                unsigned char act = active[node];
+                cnt[node] = 1;
+
+                if (1 < node && 1 == vertexes[node].size())
+                {
+                    // not root, leaf, no children
+                    dp[node][act][0][0] = 1;
+                    dp[node][act][2][1] = 1;
+                }
+                else
+                {
+                    vector<unsigned int>::iterator it = vertexes[node].begin();
+
+                    if (parents[node] == *it)
+                    {
+                        it++;
+                    }
+
+                    cnt[node] += cnt[*it];
+
+                    // first
+                    {
+                        unsigned char chact = active[*it];
+
+                        unsigned int upper = k;
+
+                        if (k > cnt[node])
+                        {
+                            upper = cnt[node];
+                        }
+
+                        for (size_t num = 0; num <= upper; num++)
+                        {
+                            // type : 0, no equipment in node, and node is not monitored
+                            dp[node][act][0][num] = dp[*it][chact][1][num];
+
+                            // type : 1,  no equipment in node, and node is monitored
+                            dp[node][act][1][num] = dp[*it][chact][3][num];
+
+                            if (0 < num)
+                            {
+                                // type : 2, equipment in node, and node is not monitored
+                                dp[node][act][2][num] = dp[*it][chact][0][num - 1] + dp[*it][chact][1][num - 1];
+                                if (MOD < dp[node][act][2][num])
+                                {
+                                    dp[node][act][2][num] %= MOD;
+                                }
+
+                                // type : 3, equipment in node, and node is monitored
+                                dp[node][act][3][num] = dp[*it][chact][2][num - 1] + dp[*it][chact][3][num - 1];
+                                if (MOD < dp[node][act][3][num])
+                                {
+                                    dp[node][act][3][num] %= MOD;
+                                }
+                            }
+                        }
+                    }
+
+                    for (it++; it != vertexes[node].end(); it++)
+                    {
+                        if (parents[node] != *it)
+                        {
+                            cnt[node] += cnt[*it];
+
+                            act = 1 - act;
+                            unsigned char chact = active[*it];
+
+                            // clear
+                            memset(dp[node][act], 0, 4 * sizeof(unsigned int) * MAX_K);
+
+                            unsigned int upper = k;
+
+                            if (k > cnt[node])
+                            {
+                                upper = cnt[node];
+                            }
+
+                            for (size_t num = 0; num <= upper; num++)
+                            {
+                                for (size_t i = 0; i <= num; i++)
+                                {
+                                    unsigned long long tmp = 0;
+                                    // type : 0, no equipment in node, and node is not monitored
+                                    tmp = dp[node][act][0][num] + (unsigned long long)dp[node][1 - act][0][i] * (unsigned long long)dp[*it][chact][1][num - i];
+                                    if (MOD < tmp)
+                                    {
+                                        tmp %= MOD;
+                                    }
+                                    dp[node][act][0][num] = tmp;
+
+                                    // type : 1,  no equipment in node, and node is monitored
+                                    tmp = dp[node][act][1][num] + (unsigned long long)dp[node][1 - act][0][i] * (unsigned long long)dp[*it][chact][3][num - i] + (unsigned long long)dp[node][1 - act][1][i] * (unsigned long long)(dp[*it][chact][3][num - i] + dp[*it][chact][1][num - i]);
+                                    if (MOD < tmp)
+                                    {
+                                        tmp %= MOD;
+                                    }
+                                    dp[node][act][1][num] = tmp;
+
+                                    // type : 2, equipment in node, and node is not monitored
+                                    tmp = dp[node][act][2][num] + (unsigned long long)dp[node][1 - act][2][i] * (unsigned long long)(dp[*it][chact][0][num - i] +
+                                                                                                                                     dp[*it][chact][1][num - i]);
+                                    if (MOD < tmp)
+                                    {
+                                        tmp %= MOD;
+                                    }
+                                    dp[node][act][2][num] = tmp;
+
+                                    // type : 3, equipment in node, and node is monitored
+                                    tmp = dp[node][act][3][num] + (unsigned long long)dp[node][1 - act][2][i] * (unsigned long long)(dp[*it][chact][2][num - i] + dp[*it][chact][3][num - i]) + (unsigned long long)dp[node][1 - act][3][i] * (unsigned long long)(dp[*it][chact][0][num - i] + dp[*it][chact][1][num - i] + dp[*it][chact][2][num - i] + dp[*it][chact][3][num - i]);
+                                    if (MOD < tmp)
+                                    {
+                                        tmp %= MOD;
+                                    }
+                                    dp[node][act][3][num] = tmp;
+                                }
+                            }
+                        }
+                    }
+
+                    active[node] = act;
+                }
+        }
+
+        nodes.pop();
+    }
+
+    unsigned int ans = 0;
 
     ans = dp[1][active[1]][1][k] + dp[1][active[1]][3][k];
-    ans %= MOD;
 
-    printf("%llu\n", ans);
+    printf("%u\n", ans);
 
 #if DEBUG
     fclose(fp);

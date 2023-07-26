987b1c3b03f49562965bf47a23e11c4195e45936
吴星煜 <wuxingyu@wuxingyudeMacBook-Air.local>
2023-07-26 18:56:20 +0800

    init

--- Luogu/P2337/main.cc
+++ Luogu/P2337/main.cc
@@ -4,7 +4,7 @@
 #include <cmath>
 #include <cstdio>
 #include <vector>
-#include <unordered_map>
+#include <map>
 #include <set>
 #include <iostream>
 #include <algorithm>
@@ -29,9 +29,27 @@ using namespace std;
 #define MAX_K   15
 #define BITS    3
 #define MASK    7
+#define BLOCK   7
+
+#define getState(ST, POS) ((ST) >> (POS * BITS)) & MASK
+
+#define setState(ST, POS, VAL)         \
+    ST &= ~(((MASK)) << (POS * BITS)); \
+    ST |= ((VAL)) << (POS * BITS);
+
+#define insertState(IDX, K, ST, CNT)                        \
+    {                                                       \
+        map<int, int>::iterator it = cnts[IDX][K].find(ST); \
+        if (it == cnts[IDX][K].end() || CNT > it->second)   \
+        {                                                   \
+            cnts[IDX][K][ST] = CNT;                         \
+        }                                                   \
+    }
 
 int n, m, k;
 char cells[MAX_N][MAX_M];
+map<int, int> cnts[2][MAX_K];           // key - state, value - state 的最大打击数
+int act = 0;                           // 当前生效的 map
 
 int main()
 {
@@ -61,7 +79,99 @@ int main()
         swap(n, m);
     }
 
-    
+    // init
+    cnts[act][0][0] = 0;
+    int now_x = 0;
+    int now_y = m;
+
+    while (true)
+    {
+        int nAct = 1 - act;
+        if (m == now_y)
+        {
+            now_x++;
+            now_y = 1;
+
+            if (n < now_x)
+            {
+                // finished 
+                break;
+            }
+        }
+        else
+        {
+            now_y++;
+        }
+
+        // 处理主体
+        for (size_t iK = 0; iK <= k; iK++)
+        {
+            for (map<int, int>::iterator it = cnts[act][iK].begin(); it != cnts[act][iK].end(); it++)
+            {
+                int state = it->first;
+                int cnt = it->second;
+
+                if (1 == now_y)
+                {
+                    state <<= 2 * BITS;
+                }
+
+                int leftPos = now_y - 1;
+                int leftUpPos = now_y;
+                int upPos = now_y + 1;
+                int rightUpPos = now_y + 2;
+
+                int leftSt = BLOCK, leftUpSt = BLOCK, upSt = BLOCK, rightUpSt = BLOCK;
+
+                if (1 < now_y)
+                {
+                    leftSt = getState(state, leftPos);
+                }
+                
+                if (1 < now_y && 1 < now_x)
+                {
+                    leftUpSt = getState(state, leftUpPos);
+                }
+
+                if (1 < now_x)
+                {
+                    upSt = getState(state, upPos);
+                }
+
+                if (1 < now_x && m > now_y)
+                {
+                    rightUpSt = getState(state, rightUpPos);
+                }
+
+                if ('#' == cells[now_x][now_y])
+                {
+                    // 障碍物
+                    int st = state;
+                    setState(st, leftPos, BLOCK);
+                    setState(st, leftUpPos, BLOCK);
+
+                    insertState(nAct, iK, st, cnt);
+                }
+                else if ('S' == cells[now_x][now_y])
+                {
+                    // 起点
+                }
+                else if ('T' == cells[now_x][now_y])
+                {
+                    // 终点
+                }
+                else
+                {
+
+                }
+            }
+
+            cnts[act][iK].clear();
+        }
+
+        // 准备下一轮
+        act = nAct;
+    }
 
     return 0;
 }

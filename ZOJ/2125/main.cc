6289bd46d616ffb3a9b92b62815c2f53ed035e11
吴星煜 <wuxingyu@wuxingyudeMacBook-Air.local>
2023-10-07 23:45:22 +0800

    init

---
+++ ZOJ/2125/main.cc
@@ -0,0 +1,35 @@
+// https://vjudge.net/problem/ZOJ-2125
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <set>
+#include <unordered_map>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <numeric>
+#include <queue>
+#include <stack>
+#include <climits>
+#include <cfloat>
+#include <limits>
+#include <bitset>
+
+using namespace std;
+
+#define ST1_BITS 3
+#define ST1_MASK 7
+#define ST2_BITS 4
+#define ST2_MASK 15
+#define QS_SIZE 600000
+
+int main()
+{
+
+    return 0;
+}

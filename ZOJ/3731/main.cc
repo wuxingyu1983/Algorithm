2b452a03782040f5fade59427adbf2dda11f63f3
吴星煜 <wuxingyu@wuxingyudeMacBook-Air.local>
2023-11-09 22:01:04 +0800

    init

--- ZOJ/3731/main.cc
+++ ZOJ/3731/main.cc
@@ -1,4 +1,6 @@
 // https://pintia.cn/problem-sets/91827364500/exam/problems/91827369576?type=7&page=27
+// https://vjudge.net/problem/HDU-4796
+// https://acm.hdu.edu.cn/showproblem.php?pid=4796
 
 #include <cmath>
 #include <cstdio>
@@ -44,7 +46,9 @@ class Record
 {
 public:
     unsigned int state; // 轮廓线段状态
-    int sum;
+    unsigned int vrts;  // 垂直插头个数
+    bool have3;         // 是否已经出现 3 插头
+    unsigned int sum;
 
     Record() {}
 };
@@ -66,31 +70,25 @@ inline void init()
     cnts[0].clear();
     cnts[1].clear();
 
-    for (int col = 1; col <= w; col++)
-    {
-        if ('0' <= cells[1][col] && '9' >= cells[1][col])
-        {
-            unsigned int st = 0;
+    qs[act][0].state = 0;
+    qs[act][0].vrts = 0;
+    qs[act][0].have3 = false;
+    qs[act][0].sum = 0;
 
-            setVal4St(st, st, (col - 1), 3);
-
-            qs[act][qTail[act]].state = st;
-            qs[act][qTail[act]].sum = 0;
-
-            qTail[act]++;
-        }
-    }
+    qTail[act]++;
 
     now_x = 0;
     now_y = w;
 }
 
-inline void addST(unsigned int st, int sum, int idx)
+inline void addST(unsigned int st, unsigned int vrts, bool have3, unsigned int sum, int idx)
 {
     unordered_map<unsigned int, unsigned int>::iterator it = cnts[idx].find(st);
     if (it == cnts[idx].end())
     {
         qs[idx][qTail[idx]].state = st;
+        qs[idx][qTail[idx]].vrts = vrts;
+        qs[idx][qTail[idx]].have3 = have3;
         qs[idx][qTail[idx]].sum = sum;
 
         cnts[idx][st] = qTail[idx];
@@ -131,6 +129,7 @@ int main()
                 if (h < now_x)
                 {
                     // finished
+                    // TBD
                     for (int iQ = 0; iQ < qTail[act]; iQ++)
                     {
                         if (qs[act][iQ].state)
@@ -152,260 +151,87 @@ int main()
             for (int iQ = 0; iQ < qTail[act]; iQ++)
             {
                 unsigned int st = qs[act][iQ].state;
-                int sum = qs[act][iQ].sum;
+                unsigned int vrts = qs[act][iQ].vrts;
+                bool have3 = qs[act][iQ].have3;
+                unsigned int sum = qs[act][iQ].sum;
 
                 if (1 == now_y)
                 {
+                    if (1 < now_x && false == have3)
+                    {
+                        continue;
+                    }
+
                     st <<= ST_BITS;
+                    vrts = 0;
                 }
 
                 if ('#' == cells[now_x][now_y])
                 {
+                    // 障碍物
                     // do nothing
-                    addST(st, sum, nAct);
+                    addST(st, vrts, have3, sum, nAct);
                 }
                 else if ('W' == cells[now_x][now_y])
                 {
                     // check
-                    int cnt = 0;
-                    for (int i = 0; i < now_y - 1; i++)
+                    if (0 == (vrts & 1))
                     {
-                        if (0 < (getVal4St(st, i)))
-                        {
-                            cnt++;
-                        }
-                    }
-
-                    if (0 == (cnt & 1))
-                    {
-                        addST(st, sum, nAct);
+                        addST(st, vrts, have3, sum, nAct);
                     }
                 }
                 else if ('L' == cells[now_x][now_y])
                 {
-                    // check
-                    int cnt = 0;
-                    for (int i = 0; i < now_y - 1; i++)
+                    if (1 == (vrts & 1))
                     {
-                        if (0 < (getVal4St(st, i)))
-                        {
-                            cnt++;
-                        }
-                    }
-
-                    if (1 == (cnt & 1))
-                    {
-                        addST(st, sum, nAct);
+                        addST(st, vrts, have3, sum, nAct);
                     }
                 }
                 else
                 {
-                    int left = 0, up = 0;
-                    left = getVal4St(st, now_y - 1);
-                    up = getVal4St(st, now_y);
+                    int left = getVal4St(st, now_y - 1);
+                    int up = getVal4St(st, now_y);
 
                     if (left && up)
                     {
-                        sum += cells[now_x][now_y] - '0';
 
-                        setVal4St(st, st, (now_y - 1), 0);
-                        setVal4St(st, st, now_y, 0);
-
-                        if (1 == left && 1 == up)
-                        {
-                            int pos = now_y + 1;
-                            int s = 1;
-                            while (pos <= w)
-                            {
-                                int v = getVal4St(st, pos);
-                                if (1 == v)
-                                {
-                                    s++;
-                                }
-                                else if (2 == v)
-                                {
-                                    s--;
-                                    if (0 == s)
-                                    {
-                                        setVal4St(st, st, pos, 1);
-                                        addST(st, sum, nAct);
-                                        break;
-                                    }
-                                }
-
-                                pos++;
-                            }
-                        }
-                        else if (2 == left && 2 == up)
-                        {
-                            int pos = now_y - 2;
-                            int s = 1;
-                            while (0 <= pos)
-                            {
-                                int v = getVal4St(st, pos);
-                                if (2 == v)
-                                {
-                                    s++;
-                                }
-                                else if (1 == v)
-                                {
-                                    s--;
-                                    if (0 == s)
-                                    {
-                                        setVal4St(st, st, pos, 2);
-                                        addST(st, sum, nAct);
-                                        break;
-                                    }
-                                }
-
-                                pos--;
-                            }
-                        }
-                        else if (1 == left && 2 == up)
-                        {
-                            // invalid
-                        }
-                        else if (2 == left && 1 == up)
-                        {
-                            addST(st, sum, nAct);
-                        }
-                        else if (3 == left && 3 == up)
-                        {
-                            // invalid
-                        }
-                        else if ((3 == left && 1 == up) || (1 == left && 3 == up))
-                        {
-                            int pos = now_y + 1;
-                            int s = 1;
-                            while (pos <= w)
-                            {
-                                int v = getVal4St(st, pos);
-                                if (1 == v)
-                                {
-                                    s++;
-                                }
-                                else if (2 == v)
-                                {
-                                    s--;
-                                    if (0 == s)
-                                    {
-                                        setVal4St(st, st, pos, 3);
-                                        addST(st, sum, nAct);
-                                        break;
-                                    }
-                                }
-
-                                pos++;
-                            }
-                        }
-                        else if ((3 == left && 2 == up) || (2 == left && 3 == up))
-                        {
-                            int pos = now_y - 2;
-                            int s = 1;
-                            while (0 <= pos)
-                            {
-                                int v = getVal4St(st, pos);
-                                if (2 == v)
-                                {
-                                    s++;
-                                }
-                                else if (1 == v)
-                                {
-                                    s--;
-                                    if (0 == s)
-                                    {
-                                        setVal4St(st, st, pos, 3);
-                                        addST(st, sum, nAct);
-                                        break;
-                                    }
-                                }
-
-                                pos--;
-                            }
-                        }
                     }
-                    else if (left)
+                    else if (left || up)
                     {
-                        sum += cells[now_x][now_y] - '0';
+                        int val = left + up;
+                        if (3 == val && now_x == h)
+                        {
+                            unsigned int newSt = st;
+                            setVal4St(newSt, newSt, now_y - 1, 0);
+                            setVal4St(newSt, newSt, now_y, 0);
 
-                        if (h == now_x && 3 == left)
-                        {
-                            addST(st, sum, nAct);
+                            addST(st, vrts + 1, have3, sum + (cells[now_x][now_y] - '0'), nAct);
                         }
-                        else if (h > now_x)
+                        else
                         {
-                            if ('0' <= cells[now_x + 1][now_y] && '9' >= cells[now_x + 1][now_y])
+                            if (h > now_x && '#' != cells[now_x + 1][now_y] && 'W' != cells[now_x + 1][now_y] && 'L' != cells[now_x + 1][now_y])
                             {
-                                addST(st, sum, nAct);
-                            }
-                        }
+                                unsigned int newSt = st;
+                                setVal4St(newSt, newSt, now_y - 1, val);
+                                setVal4St(newSt, newSt, now_y, 0);
 
-                        if (w > now_y)
-                        {
-                            if ('0' <= cells[now_x][now_y + 1] && '9' >= cells[now_x][now_y + 1])
+                                addST(st, vrts + 1, have3, sum + (cells[now_x][now_y] - '0'), nAct);
+                            }
+
+                            if (w > now_y && '#' != cells[now_x][now_y + 1] && 'W' != cells[now_x][now_y + 1] && 'L' != cells[now_x][now_y + 1])
                             {
-                                setVal4St(st, st, (now_y - 1), 0);
-                                setVal4St(st, st, now_y, left);
+                                unsigned int newSt = st;
+                                setVal4St(newSt, newSt, now_y - 1, 0);
+                                setVal4St(newSt, newSt, now_y, val);
 
-                                addST(st, sum, nAct);
+                                addST(st, vrts + 1, have3, sum + (cells[now_x][now_y] - '0'), nAct);
                             }
-                        }
-                    }
-                    else if (up)
-                    {
-                        sum += cells[now_x][now_y] - '0';
-
-                        if (w > now_y)
-                        {
-                            if ('0' <= cells[now_x][now_y + 1] && '9' >= cells[now_x][now_y + 1])
-                            {
-                                addST(st, sum, nAct);
-                            }
-                        }
-
-                        if (h == now_x && 3 == up)
-                        {
-                            setVal4St(st, st, (now_y - 1), up);
-                            setVal4St(st, st, now_y, 0);
-
-                            addST(st, sum, nAct);
-                        }
-                        else if (h > now_x)
-                        {
-                            if ('0' <= cells[now_x + 1][now_y] && '9' >= cells[now_x + 1][now_y])
-                            {
-                                setVal4St(st, st, (now_y - 1), up);
-                                setVal4St(st, st, now_y, 0);
-
-                                addST(st, sum, nAct);
-                            }
-                        }
+                        }                        
                     }
                     else
                     {
                         // 0 == left && 0 == up
-                        // do nothing
-                        addST(st, sum, nAct);
 
-                        sum += cells[now_x][now_y] - '0';
-
-                        if (h > now_x && w > now_y)
-                        {
-                            if ('#' == cells[now_x + 1][now_y] || 'W' == cells[now_x + 1][now_y] || 'L' == cells[now_x + 1][now_y])
-                            {
-                                continue;
-                            }
-
-                            if ('#' == cells[now_x][now_y + 1] || 'W' == cells[now_x][now_y + 1] || 'L' == cells[now_x][now_y + 1])
-                            {
-                                continue;
-                            }
-
-                            setVal4St(st, st, (now_y - 1), 1);
-                            setVal4St(st, st, now_y, 2);
-
-                            addST(st, sum, nAct);
-                        }
                     }
                 }
             }

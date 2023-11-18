eb9e8eae7d350b77f1fbc57c52d3e2a264d7e4da
吴星煜 <wuxingyu@wuxingyudeMacBook-Air.local>
2023-11-18 22:57:12 +0800

    init

--- Luogu/P3170/main.cc
+++ Luogu/P3170/main.cc
@@ -52,6 +52,24 @@ int now_x, now_y;
     if (VAL)                               \
         ST |= (VAL) << ((POS) * ST_BITS);
 
+#define addSts(ST, LS, CNT, IDX)                                                         \
+    {                                                                                    \
+        unordered_map<unsigned int, unsigned int>::iterator it = cnts[IDX][LS].find(ST); \
+        if (it == cnts[IDX][LS].end())                                                   \
+        {                                                                                \
+            int pInQ = qTail[IDX];                                                       \
+            qs[IDX][pInQ].state = ST;                                                    \
+            qs[IDX][pInQ].ls = LS;                                                       \
+            qs[IDX][pInQ].cnt = CNT;                                                     \
+            cnts[IDX][LS][ST] = pInQ;                                                    \
+            qTail[IDX]++;                                                                \
+        }                                                                                \
+        else                                                                             \
+        {                                                                                \
+            qs[IDX][it->second].cnt += CNT;                                              \
+        }                                                                                \
+    }
+
 inline void init()
 {
     // 每一个 test 前，初始化
@@ -83,8 +101,128 @@ int main()
     }
 
     init();
+    unsigned long long ans = 0;
 
-    
+    while (0 < qTail[act])
+    {
+        int nAct = 1 - act;
+
+        if (w == now_y)
+        {
+            now_x++;
+            now_y = 1;
+
+            if (h < now_x)
+            {
+                // finished
+                break;
+            }
+        }
+        else
+        {
+            now_y++;
+        }
+
+        for (size_t iQ = 0; iQ < qTail[act]; iQ++)
+        {
+            unsigned int st = qs[act][iQ].state;
+            unsigned int ls = qs[act][iQ].ls;
+            unsigned long long cnt = qs[act][iQ].cnt;
+
+            if (1 == now_y)
+            {
+                st <<= ST_BITS;
+            }
+
+            if ('#' == cells[now_x][now_y])
+            {
+                // 障碍物
+                addSts(st, ls, cnt, nAct);
+            }
+            else
+            {
+                int left = getVal4St(st, now_y - 1);
+                int up = getVal4St(st, now_y);
+
+                if (left && up)
+                {
+                    // 非法
+                }
+                else if (left)
+                {
+                    {
+                        // 截断
+                        unsigned int newSt = st;
+                        setVal4St(newSt, now_y - 1, 0);
+                        setVal4St(newSt, now_y, 0);
+
+                        if (3 == ls && 0 == newSt)
+                        {
+                            ans += cnt;
+                        }
+                        else
+                        {
+                            addSts(newSt, ls, cnt, nAct);
+                        }
+                    }
+
+                    if (w > now_y && '.' == cells[now_x][now_y + 1])
+                    {
+                        unsigned int newSt = st;
+                        setVal4St(newSt, now_y - 1, 0);
+                        setVal4St(newSt, now_y, 1);
+
+                        addSts(newSt, ls, cnt, nAct);
+                    }
+                }
+                else if (up)
+                {
+                    if (h > now_x && '.' == cells[now_x + 1][now_y])
+                    {
+                        unsigned int newSt = st;
+                        setVal4St(newSt, now_y - 1, 1);
+                        setVal4St(newSt, now_y, 0);
+
+                        addSts(newSt, ls, cnt, nAct);
+                    }
+
+                    if (w > now_y && '.' == cells[now_x][now_y + 1])
+                    {
+                        unsigned int newSt = st;
+                        setVal4St(newSt, now_y - 1, 0);
+                        setVal4St(newSt, now_y, 1);
+
+                        addSts(newSt, ls, cnt, nAct);
+                    }
+                }
+                else
+                {
+                    // 0 == left && 0 == up
+                    // do nothing
+                    addSts(st, ls, cnt, nAct);
+
+                    // new L
+                    if (3 > ls && h > now_x && '.' == cells[now_x + 1][now_y])
+                    {
+                        unsigned int newSt = st;
+                        setVal4St(newSt, now_y - 1, 1);
+                        setVal4St(newSt, now_y, 0);
+
+                        addSts(newSt, ls + 1, cnt, nAct);
+                    }
+                }
+            }
+        }
+
+        qTail[act] = 0;
+        cnts[act][0].clear();
+        cnts[act][1].clear();
+        cnts[act][2].clear();
+        cnts[act][3].clear();
+        act = nAct;
+    }
+
+    cout << ans << endl;
 
     return 0;
 }

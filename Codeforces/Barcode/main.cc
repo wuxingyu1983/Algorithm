515595d551535e34d8cf8c45a21a48a22ea248b9
吴星煜 <wuxingyu@wuxingyudeMacBook-Air.local>
2023-12-16 16:05:55 +0800

    Accepted

--- Codeforces/Barcode/main.cc
+++ Codeforces/Barcode/main.cc
@@ -28,10 +28,10 @@ using namespace std;
 #define WHITE 0
 #define BLACK 1
 
-unsigned int h, w, x, y;
-unsigned int cnts[MAX_W][2];    // 某一列 白、黑 子的个数
-unsigned int sums[MAX_W][2];    // 1 - col 列 白、黑 子的总数
-unsigned int mins[MAX_W][2];    // 第 col 列为全 白 或 黑 子的最小改动数目
+int h, w, x, y;
+unsigned int cnts[MAX_W][2]; // 某一列 白、黑 子的个数
+unsigned int sums[MAX_W][2]; // 1 - col 列 白、黑 子的总数
+unsigned int mins[MAX_W][2]; // 第 col 列为全 白 或 黑 子的最小改动数目
 
 int main()
 {
@@ -47,25 +47,60 @@ int main()
             if ('.' == ch)
             {
                 // white
-                cnts[col][WHITE] ++;
+                cnts[col][WHITE]++;
             }
             else
             {
                 // black
-                cnts[col][BLACK] ++;
+                cnts[col][BLACK]++;
             }
         }
     }
-    
+
     for (size_t col = 1; col <= w; col++)
     {
         sums[col][WHITE] = sums[col - 1][WHITE] + cnts[col][WHITE];
         sums[col][BLACK] = sums[col - 1][BLACK] + cnts[col][BLACK];
 
-        
+        // init
+        mins[col][WHITE] = INT_MAX;
+        mins[col][BLACK] = INT_MAX;
     }
 
+    for (int col = 1; col <= w; col++)
+    {
+        for (int j = col - y + 1; j <= col - x + 1; j++)
+        {
+            if (0 < j)
+            {
+                // white - mins[col][WHITE]
+                if (INT_MAX > mins[j - 1][BLACK])
+                {
+                    if (mins[col][WHITE] > (mins[j - 1][BLACK] + sums[col][WHITE] - sums[j - 1][WHITE]))
+                    {
+                        mins[col][WHITE] = mins[j - 1][BLACK] + sums[col][WHITE] - sums[j - 1][WHITE];
+                    }
+                }
 
+                // black - mins[col][BLACK]
+                if (INT_MAX > mins[j - 1][WHITE])
+                {
+                    if (mins[col][BLACK] > (mins[j - 1][WHITE] + sums[col][BLACK] - sums[j - 1][BLACK]))
+                    {
+                        mins[col][BLACK] = mins[j - 1][WHITE] + sums[col][BLACK] - sums[j - 1][BLACK];
+                    }
+                }
+            }
+        }
+    }
+
+    unsigned int ans = mins[w][WHITE];
+    if (ans > mins[w][BLACK])
+    {
+        ans = mins[w][BLACK];
+    }
+
+    cout << ans << endl;
 
     return 0;
 }

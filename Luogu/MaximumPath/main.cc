249c32c18c5b6bbdf12cc6e6f40b1963da4fcd3e
LeoNg83 <85401996@qq.com>
2024-03-30 23:10:14 +0800

    init

--- Luogu/MaximumPath/main.cc
+++ Luogu/MaximumPath/main.cc
@@ -40,7 +40,7 @@ public:
 long long cells[MAX_N][4];
 Record qs[2][QS_SIZE];
 int qTail[2];
-int n;
+int n;      // row
 unordered_map<unsigned int, unsigned int> cnts[2];
 int act = 0; // 当前生效的 map
 int now_x, now_y;
@@ -71,14 +71,65 @@ int main()
     act = 0;
 
     now_x = 0;
-    now_y = n;
+    now_y = 3;
 
     qs[act][0].state = 0;
     qs[act][0].sum = 0;
 
     qTail[act]++;
 
+    while (0 < qTail[act])
+    {
+        int nAct = 1 - act;
 
+        if (3 == now_y)
+        {
+            now_x++;
+            now_y = 1;
+
+            if (n < now_x)
+            {
+                // TBD
+                break;
+            }
+        }
+        else
+        {
+            now_y++;
+        }
+
+        for (size_t iQ = 0; iQ < qTail[act]; iQ++)
+        {
+            unsigned int st = qs[act][iQ].state;
+            long long sum = qs[act][iQ].sum;
+
+            if (1 == now_y)
+            {
+                st <<= ST_BITS;
+            }
+
+            unsigned int left = getVal4St(st, now_y - 1);
+            unsigned int up = getVal4St(st, now_y);
+
+            if (1 == now_x && 1 == now_y)
+            {
+                // 起点
+                
+            }
+            else if (n == now_x && 3 == now_y)
+            {
+                // 终点
+            }
+            else
+            {
+
+            }
+        }
+
+        qTail[act] = 0;
+        cnts[act].clear();
+        act = nAct;
+    }
 
     return 0;
 }

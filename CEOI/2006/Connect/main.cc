526647aa4b65ce7d6df2b2301136248fa1e87f65
LeoNg83 <85401996@qq.com>
2024-04-05 19:31:30 +0800

    init

--- CEOI/2006/Connect/main.cc
+++ CEOI/2006/Connect/main.cc
@@ -33,6 +33,7 @@ class Record
 public:
     unsigned int state;
     unsigned int score;
+    char cached[MAX_H][MAX_W];
 
     Record() {}
 };
@@ -52,9 +53,37 @@ int now_x, now_y;
     if (VAL)                               \
         ST |= (VAL) << ((POS) * ST_BITS);
 
+#define addSts(ST, CNT, REDS, IDX)                                                          \
+    unsigned char unused;                                                                   \
+    unsigned int recodedSt = ST;                                                            \
+    recode(recodedSt, unused);                                                              \
+    if (REDS == k)                                                                          \
+    {                                                                                       \
+        if (3 > unused)                                                                     \
+            ans += CNT;                                                                     \
+    }                                                                                       \
+    else                                                                                    \
+    {                                                                                       \
+        unordered_map<unsigned int, unsigned int>::iterator it = cnts[IDX].find(recodedSt); \
+        if (it == cnts[IDX].end())                                                          \
+        {                                                                                   \
+            int pInQ = qTail[IDX];                                                          \
+            qs[IDX][pInQ].state = recodedSt;                                                \
+            qs[IDX][pInQ].count = CNT;                                                      \
+            qs[IDX][pInQ].minUnused = unused;                                               \
+            cnts[IDX][recodedSt] = pInQ;                                                    \
+            qTail[IDX]++;                                                                   \
+        }                                                                                   \
+        else                                                                                \
+        {                                                                                   \
+            qs[IDX][it->second].count += CNT;                                               \
+        }                                                                                   \
+    }
+
+
 int main()
 {
-    bool bSwitch = false;    
+    bool bSwitch = false;
 
     cin >> h >> w;
 
@@ -80,6 +109,90 @@ int main()
                 cin >> cells[row][col];
             }
         }
+    }
+
+    // init
+    act = 0;
+
+    now_x = 2;
+    now_y = 2;
+
+    qs[act][0].state = 0;
+    qs[act][0].score = 0;
+    memcpy(qs[act]->cached, cells, sizeof(cells));
+
+    qTail[act]++;
+
+    while (0 < qTail[act])
+    {
+        int nAct = 1 - act;
+
+        if ((w - 1) == now_y)
+        {
+            now_x += 2;
+            now_y = 2;
+
+            if (h < now_x)
+            {
+                // TBD
+                break;
+            }
+        }
+        else
+        {
+            now_y += 2;
+        }
+
+        for (size_t iQ = 0; iQ < qTail[act]; iQ++)
+        {
+            unsigned int st = qs[act][iQ].state;
+            unsigned int score = qs[act][iQ].score;
+
+            if (1 == (now_y >> 1))
+            {
+                st <<= ST_BITS;
+            }
+
+            unsigned int left = getVal4St(st, (now_y >> 1) - 1);
+            unsigned int up = getVal4St(st, (now_y >> 1));
+
+            if (left && up)
+            {
+                if ('X' == cells[now_x][now_y])
+                {
+                    // invalid
+                }
+                else
+                {
+
+                }
+            }
+            else if (left || up)
+            {
+                if ('X' == cells[now_x][now_y])
+                {
+                }
+                else
+                {
+                }
+            }
+            else
+            {
+                // 0 == left && 0 == up
+                if ('X' == cells[now_x][now_y])
+                {
+                }
+                else
+                {
+                    // do nonthing
+
+                }
+            }
+        }
+
+        qTail[act] = 0;
+        cnts[act].clear();
+        act = nAct;
     }
 
     return 0;

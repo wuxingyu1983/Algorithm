333222b6700699c991ea814e38e49e7bbd0ab9e7
LeoNg83 <85401996@qq.com>
2024-04-28 17:25:43 +0900

    init

---
+++ Luogu/P3694/main.cc
@@ -0,0 +1,45 @@
+// https://www.luogu.com.cn/problem/P3694
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <numeric>
+#include <queue>
+#include <stack>
+#include <climits>
+#include <cfloat>
+#include <limits>
+#include <bitset>
+#include <cstring>
+
+using namespace std;
+
+#define MAX_N 100001
+#define MAX_M 21
+#define MAX_2M 1048577
+
+unsigned int n, m;
+unsigned char idols[MAX_N];
+int dp[MAX_2M];
+
+int main()
+{
+    cin >> n >> m;
+
+    for (size_t i = 0; i < n; i++)
+    {
+        cin >> idols[i];
+    }
+
+
+
+    return 0;
+}

ea4495ddb7dd31067b2f4be8d27f86bf0546227f
LeoNg83 <85401996@qq.com>
2024-05-03 22:05:54 +0900

    init

---
+++ Luogu/P3959/main.cc
@@ -0,0 +1,46 @@
+// https://www.luogu.com.cn/problem/P3959
+// 状压DP
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <numeric>
+#include <queue>
+#include <stack>
+#include <climits>
+#include <cfloat>
+#include <limits>
+#include <bitset>
+#include <cstring>
+
+using namespace std;
+
+#define MAX_N   12
+#define MAX_M   1000
+
+class Line
+{
+public:
+    int point;
+    int road;
+
+    Line(int _point, int _road) : point(_point), road(_road) {}
+};
+
+vector<Line> lines[MAX_N + 1];
+
+int n, m;
+
+int main()
+{
+
+    return 0;
+}

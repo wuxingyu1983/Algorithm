4af003beb69229e0a36eec70a86d6f90546e2b2a
LeoNg83 <85401996@qq.com>
2024-05-04 13:42:03 +0900

    init

--- Luogu/P3959/main.cc
+++ Luogu/P3959/main.cc
@@ -23,8 +23,10 @@
 
 using namespace std;
 
-#define MAX_N   12
-#define MAX_M   1000
+#define MAX_N 12
+#define MAX_2N 4096
+#define MAX_M 1000
+#define QS_SIZE 100000
 
 class Line
 {
@@ -36,6 +38,23 @@ public:
 };
 
 vector<Line> lines[MAX_N + 1];
+
+class Record
+{
+public:
+    int state;  // layer + st1 + st2;
+    int cost;
+    
+    Record() {}
+};
+
+Record qs[QS_SIZE];
+int qHead, qTail;
+
+#define IS_EMPTY (qHead == qTail)
+#define IS_FULL (qHead == ((qTail + 1) % QS_SIZE))
+
+unordered_map<int, int> mps[MAX_N];
 
 int n, m;
 
@@ -51,6 +70,17 @@ int main()
         lines[src].push_back(Line(dst, len));
         lines[dst].push_back(Line(src, len));
     }
+
+    // init
+    for (size_t i = 0, st = 1; i < n; i++, st <<= 1)
+    {
+        qs[qTail].state = st | (st << n);
+        qTail++;
+    }
+
+    int ans = -1;
+
+    cout << ans << endl;
 
     return 0;
 }

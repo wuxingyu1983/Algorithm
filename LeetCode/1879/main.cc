76e35072647e8dff9a7dfdc55440d6b68949eabb
LeoNg83 <85401996@qq.com>
2024-06-25 14:12:31 +0800

    AC

--- LeetCode/1879/main.cc
+++ LeetCode/1879/main.cc
@@ -13,11 +13,62 @@
 
 using namespace std;
 
+#define MAX_N2  16384
+
 class Solution {
 public:
     int minimumXORSum(vector<int>& nums1, vector<int>& nums2) {
+        n = nums1.size();
+        init();
 
+        for (size_t i = 0; i < n; i++)
+        {
+            for (vector<int>::iterator it = digits[i].begin(); it != digits[i].end(); it++)
+            {
+                for (size_t b = 0; b < n; b++)
+                {
+                    if (0 == (*it & (1 << b)))
+                    {
+                        int newI = *it | (1 << b);
+                        int newSum = dp[*it] + (nums1[i] ^ nums2[b]);
+
+                        if (0 > dp[newI] || dp[newI] > newSum)
+                        {
+                            dp[newI] = newSum;
+                        }
+                    }
+                }
+            }
+        }
+
+        return dp[(1 << n) - 1];
     }
+
+private:
+    void init() {
+        memset(dp, -1, sizeof(dp));
+        dp[0] = 0;
+
+        digits[0].push_back(0);
+
+        for (size_t i = 1; i < (1 << n); i++)
+        {
+            int cnt = 0;
+            for (size_t bit = 1; bit <= i; bit <<= 1)
+            {
+                if (i & bit)
+                {
+                    cnt ++;
+                }
+            }
+
+            digits[cnt].push_back(i);
+        }
+    }
+
+    int dp[MAX_N2];
+    int n;
+    vector<int> digits[15];
 };
 
 int main()

8b00ef15ba8cb9981c2d24b7dd6ff64f41560af8
LeoNg83 <85401996@qq.com>
2024-07-19 09:15:31 +0000

    init

--- LeetCode/1434/main.cc
+++ LeetCode/1434/main.cc
@@ -16,11 +16,41 @@ using namespace std;
 class Solution {
 public:
     int numberWays(vector<vector<int>>& hats) {
+        // init
+        mod = 1000000007;
+        n = hats.size();
+        int finalSt = (1 << n) - 1;
+        memset(dp, -1, sizeof(dp));
 
+        for (size_t i = 0; i < n; i++)
+        {
+            for (vector<int>::iterator it = hats[i].begin(); it != hats[i].end(); it++)
+            {
+                likes[*it].push_back(i);
+            }
+        }
+
+        dp[0][0] = 1;
+
+        for (size_t h = 1; h <= 40; h++)
+        {
+            for (size_t st = 0; st <= finalSt; st++)
+            {
+                if (0 <= dp[h -1][st])
+                {
+
+                }
+            }
+        }
+        
+        return dp[40][finalSt];
     }
 
 private:
-    
+    int n;
+    int mod;
+    vector<int> likes[41];
+    int dp[41][1024];
 };
 
 int main()

76e6cceccc45c16ef8adefd562e07e8250bbc304
LeoNg83 <85401996@qq.com>
2024-07-25 16:19:54 +0000

    init

---
+++ LeetCode/1799/main.cc
@@ -0,0 +1,65 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int maxScore(vector<int> &nums)
+    {
+        init(nums);
+    }
+
+private:
+    int gcd[14][14];
+    int dp[8][16384];
+
+    void init(vector<int> &nums)
+    {
+        memset(gcd, 0, sizeof(gcd));
+        memset(dp, 0, sizeof(dp));
+
+
+    }
+
+    int gcdFunc(int a, int b)
+    {
+        for (;;)
+        {
+            if (b == 0)
+                return a;
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+    }
+};
+
+int main()
+{
+    vector<int> nums;
+
+    int num;
+    while (cin >> num)
+    {
+        nums.push_back(num);
+        if (cin.get() == '\n')
+            break;
+    }
+
+    Solution s;
+    cout << s.maxScore(nums) << endl;
+
+    return 0;
+}

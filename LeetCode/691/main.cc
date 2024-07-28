4b171e092b82a5f4fe4a861c4521f99df543c79f
LeoNg83 <85401996@qq.com>
2024-07-28 07:37:04 +0000

    init

--- LeetCode/691/main.cc
+++ LeetCode/691/main.cc
@@ -19,7 +19,27 @@ public:
     int minStickers(vector<string> &stickers, string target)
     {
         // init
+        finalSt = (1 << target.length()) - 1;
+        dp = new int *[stickers.size() + 1];
+        for (size_t i = 0; i <= stickers.size(); i++)
+        {
+            dp[i] = new int[finalSt + 1];
+            memset(dp[i], -1, sizeof(int) * (finalSt + 1));
+        }
+        dp[0][0] = 0;
 
+        for (size_t i = 1; i <= stickers.size(); i++)
+        {
+            for (size_t st = 0; st <= finalSt; st++)
+            {
+                if (0 <= dp[i - 1][st])
+                {
+                    
+                }
+            }
+        }
+
+        return dp[stickers.size()][finalSt];
     }
 
 private:

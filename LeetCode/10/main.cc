cc60c7c9884bbfcce55515d280f3fec7760752de
LeoNg83 <85401996@qq.com>
2024-07-28 15:41:38 +0000

    init

---
+++ LeetCode/10/main.cc
@@ -0,0 +1,34 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isMatch(string s, string p)
+    {
+    }
+};
+
+int main()
+{
+    string str, pat;
+    cin >> str;
+    cin >> pat;
+
+    Solution s;
+    cout << s.isMatch(str, pat) << endl;
+
+    return 0;
+}

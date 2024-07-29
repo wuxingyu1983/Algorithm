12a9801d090a5557930d21daf7e9148a6bae5991
LeoNg83 <85401996@qq.com>
2024-07-29 16:01:40 +0000

    init

--- LeetCode/10/main.cc
+++ LeetCode/10/main.cc
@@ -18,7 +18,46 @@ class Solution
 public:
     bool isMatch(string s, string p)
     {
+        bool bRet = false;
+
+        memset(dp, 0, sizeof(dp));
+        dp[0][0] = 1;
+
+        for (size_t iP = 1; iP <= p.length(); iP++)
+        {
+            char chP = p.at(iP - 1);
+            for (size_t iS = 1; iS <= s.length(); iS++)
+            {
+                char chS = s.at(iS - 1);
+
+                if ('*' == chP)
+                {
+                    dp[iS][iP] = dp[iS - 1][iP - 1] + dp[iS - 1][iP] + dp[iS][iP - 1];
+                }
+                else if ('.' == chP)
+                {
+                    dp[iS][iP] = dp[iS - 1][iP - 1];
+                }
+                else
+                {
+                    if (chS == chP)
+                    {
+                        dp[iS][iP] = dp[iS - 1][iP - 1];
+                    }
+                }
+            }
+        }
+
+        if (dp[s.length()][p.length()])
+        {
+            bRet = true;
+        }
+
+        return bRet;
     }
+
+private:
+    char dp[21][21];
 };
 
 int main()
@@ -28,7 +67,10 @@ int main()
     cin >> pat;
 
     Solution s;
-    cout << s.isMatch(str, pat) << endl;
+    if (s.isMatch(str, pat))
+        cout << "true" << endl;
+    else
+        cout << "false" << endl;
 
     return 0;
 }

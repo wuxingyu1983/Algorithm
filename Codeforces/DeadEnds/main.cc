74bcb41f32fb90b212cd959e17335d1daf88f80b
LeoNg83 <85401996@qq.com>
2024-08-18 07:21:51 +0000

    init

--- Codeforces/DeadEnds/main.cc
+++ Codeforces/DeadEnds/main.cc
@@ -25,8 +25,16 @@ using namespace std;
 #define DEBUG   0
 #define MAX_N   10
 #define MAX_2N  1024
+#define MAX_M   46
 
-int dp[MAX_2N][MAX_2N];     // [st1][st2], st1 : 1 - 已经包含了；st2 : 1 - dead ends
+#define getVal4St(ST, POS) (((ST) >> (POS)) & 1)
+
+#define setVal4St(ST, POS, VAL) \
+    ST &= ~(1 << (POS));        \
+    if (VAL)                    \
+        ST |= (VAL) << (POS);
+
+int dp[MAX_M][MAX_2N][MAX_2N];     // [st1][st2], st1 : 1 - 已经包含了；st2 : 1 - dead ends
 
 int n, m, k;
 
@@ -37,9 +45,10 @@ int main()
     int finalSt = (1 << n) - 1;
 
     // init
-    dp[0][0] = 1;
+    memset(dp, 0, sizeof(dp));
+    dp[0][0][0] = 1;
 
-    for (size_t iM = 0; iM < m; iM++)
+    for (size_t iM = 1; iM <= m; iM++)
     {
         int v1, v2;
         cin >> v1 >> v2;
@@ -51,15 +60,76 @@ int main()
         {
             for (size_t st2 = 0; st2 <= finalSt; st2++)
             {
-                if (dp[st1][st2])
+                if (dp[iM - 1][st1][st2])
                 {
+                    int newSt1 = st1;
+                    int newSt2 = st2;
 
+                    if (getVal4St(st1, v1) && getVal4St(st1, v2))
+                    {
+                        // v1, v2 都已经包含了，忽略该 road
+                    }
+                    else if (getVal4St(st1, v1))
+                    {
+                        // unset st2 : v1
+                        setVal4St(newSt2, v1, 0);
+
+                        // set st1,st2 : v2
+                        setVal4St(newSt1, v2, 1);
+                        setVal4St(newSt2, v2, 1);
+
+                        dp[iM][newSt1][newSt2] += dp[iM - 1][st1][st2];
+                    }
+                    else if (getVal4St(st1, v2))
+                    {
+                        // unset st2 : v2
+                        setVal4St(newSt2, v2, 0);
+
+                        // set st1,st2 : v1
+                        setVal4St(newSt1, v1, 1);
+                        setVal4St(newSt2, v1, 1);
+
+                        dp[iM][newSt1][newSt2] += dp[iM - 1][st1][st2];
+                    }
+                    else
+                    {
+                        // set st1,st2 : v1
+                        setVal4St(newSt1, v1, 1);
+                        setVal4St(newSt2, v1, 1);
+
+                        // set st1,st2 : v2
+                        setVal4St(newSt1, v2, 1);
+                        setVal4St(newSt2, v2, 1);
+
+                        dp[iM][newSt1][newSt2] += dp[iM - 1][st1][st2];
+                    }
                 }
             }
         }
     }
 
     int ans = 0;
+
+    for (size_t st2 = 0; st2 <= finalSt; st2++)
+    {
+        if (dp[m][finalSt][st2])
+        {
+            int tmp = 0;
+
+            for (size_t pos = 0; pos < n; pos ++)
+            {
+                if (getVal4St(st2, pos))
+                {
+                    tmp ++;
+                }
+            }
+
+            if (k == tmp)
+            {
+                ans += dp[m][finalSt][st2];
+            }
+        }
+    }
 
     cout << ans << endl;
 

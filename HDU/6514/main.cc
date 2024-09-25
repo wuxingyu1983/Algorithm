f41b954c669012390962e9f96c3d1de5ec44cbf2
LeoNg83 <85401996@qq.com>
2024-09-25 12:03:50 +0000

    time out

--- HDU/6514/main.cc
+++ HDU/6514/main.cc
@@ -51,6 +51,49 @@ int main()
         }
     }
     
+    for (size_t row = 0; row < n; row++)
+    {
+        for (size_t col = 1; col < m; col++)
+        {
+            dp[row * m + col] += dp[row * m + col - 1] + land[row * m + col];
+        }
+    }
+
+    for (size_t col = 0; col < m; col++)
+    {
+        for (size_t row = 1; row < n; row++)
+        {
+            dp[row * m + col] += dp[(row - 1) * m + col];
+        }
+    }
+
+    int q;
+    cin >> q;
+
+    for (size_t i = 0; i < q; i++)
+    {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+
+        x1 --;
+        y1 --;
+        x2 --;
+        y2 --;
+
+        int total = (y2 - y1 + 1) * (x2 - x1 + 1);
+        int cnt = dp[x2 * m + y2];
+        if (0 < x1)
+            cnt -= dp[(x1 - 1) * m + y2];
+        if (0 < y1)
+            cnt -= dp[x2 * m + (y1 - 1)];
+        if (0 < x1 && 0 < y1)
+            cnt += dp[(x1 - 1) * m + (y1 - 1)];
+        if (total == cnt)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    
 
     return 0;
 }

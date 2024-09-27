2a63be21bafcd2df974396921c7c356dceb92a31
LeoNg83 <85401996@qq.com>
2024-09-27 11:42:55 +0000

    add segment tree

--- HDU/6514/main.cc
+++ HDU/6514/main.cc
@@ -22,7 +22,8 @@
 
 using namespace std;
 
-#define MAX_MN  10000001
+#define MAX_MN 10000001
+#define ROOT 1
 
 char land[MAX_MN];
 int dp[MAX_MN];
@@ -43,6 +44,55 @@ vector<Monitor> vecOut;     // 出扫描线的顺序队列
 
 bool cmpIn (Monitor &a, Monitor &b) { return a.y1 < b.y1; }
 bool cmpOut (Monitor &a, Monitor &b) { return a.y2 < b.y2; }
+
+vector<int> d;
+vector<int> b;
+
+void update(int l, int r, int c, int s, int t, int p)
+{
+    // [l, r] 为修改区间, c 为被修改的元素的变化量, [s, t] 为当前节点包含的区间, p
+    // 为当前节点的编号
+    if (l <= s && t <= r)
+    {
+        d[p] += (t - s + 1) * c, b[p] += c;
+        return;
+    } // 当前区间为修改区间的子集时直接修改当前节点的值,然后打标记,结束修改
+    int m = s + ((t - s) >> 1);
+    if (b[p] && s != t)
+    {
+        // 如果当前节点的懒标记非空,则更新当前节点两个子节点的值和懒标记值
+        d[p * 2] += b[p] * (m - s + 1), d[p * 2 + 1] += b[p] * (t - m);
+        b[p * 2] += b[p], b[p * 2 + 1] += b[p]; // 将标记下传给子节点
+        b[p] = 0;                               // 清空当前节点的标记
+    }
+    if (l <= m)
+        update(l, r, c, s, m, p * 2);
+    if (r > m)
+        update(l, r, c, m + 1, t, p * 2 + 1);
+    d[p] = d[p * 2] + d[p * 2 + 1];
+}
+
+int getsum(int l, int r, int s, int t, int p)
+{
+    // [l, r] 为查询区间, [s, t] 为当前节点包含的区间, p 为当前节点的编号
+    if (l <= s && t <= r)
+        return d[p];
+    // 当前区间为询问区间的子集时直接返回当前区间的和
+    int m = s + ((t - s) >> 1);
+    if (b[p])
+    {
+        // 如果当前节点的懒标记非空,则更新当前节点两个子节点的值和懒标记值
+        d[p * 2] += b[p] * (m - s + 1), d[p * 2 + 1] += b[p] * (t - m);
+        b[p * 2] += b[p], b[p * 2 + 1] += b[p]; // 将标记下传给子节点
+        b[p] = 0;                               // 清空当前节点的标记
+    }
+    int sum = 0;
+    if (l <= m)
+        sum = getsum(l, r, s, m, p * 2);
+    if (r > m)
+        sum += getsum(l, r, m + 1, t, p * 2 + 1);
+    return sum;
+}
 
 int main()
 {
@@ -67,7 +117,41 @@ int main()
 
     // sort
     sort(vecIn.begin(), vecIn.end(), cmpIn);
-    
+
+    // segment tree
+    d.resize(4 * n, 0);
+    b.resize(4 * n, 0);
+
+    for (size_t col = 0; col < m; col++)
+    {
+        while (0 < vecIn.size() && vecIn[0].y1 == col)
+        {
+            update(vecIn[0].x1, vecIn[0].x2, 1, 0, n - 1, ROOT);
+
+            vecOut.push_back(vecIn[0]);
+            sort(vecOut.begin(), vecOut.end(), cmpOut);
+
+            vecIn.erase(vecIn.begin());
+        }
+
+        if (0 < vecOut.size())
+        {
+            for (size_t row = 0; row < n; row++)
+            {
+                if (0 < getsum(row, row, 0, n - 1, ROOT))
+                {
+                    land[row * m + col] = 1;
+                }
+            }
+        }
+
+        while (0 < vecOut.size() && vecOut[0].y2 == col)
+        {
+            update(vecOut[0].x1, vecOut[0].x2, -1, 0, n - 1, ROOT);
+            vecOut.erase(vecOut.begin());
+        }
+    }
+
     for (size_t row = 0; row < n; row++)
     {
         for (size_t col = 1; col < m; col++)

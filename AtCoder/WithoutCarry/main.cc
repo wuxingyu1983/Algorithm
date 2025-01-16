11e3f2d98bb2524c1ec6a39b757d951346d70d98
LeoNg83 <85401996@qq.com>
2025-01-16 15:12:24 +0000

    AC

--- AtCoder/WithoutCarry/main.cc
+++ AtCoder/WithoutCarry/main.cc
@@ -106,9 +106,45 @@ int main()
                         }
 
     long long ans = 0;
-    
 
-    cout << ans << endl; 
+    for (size_t i = 0; i < n; i++)
+    {
+        int tmp = a[i];
+        bool flag = true;
+
+        int x1, x2, x3, x4, x5, x6;
+        x1 = tmp % 10;
+        if (x1 >= 5)
+            flag = false;
+        tmp /= 10;
+        x2 = tmp % 10;
+        if (x2 >= 5)
+            flag = false;
+        tmp /= 10;
+        x3 = tmp % 10;
+        if (x3 >= 5)
+            flag = false;
+        tmp /= 10;
+        x4 = tmp % 10;
+        if (x4 >= 5)
+            flag = false;
+        tmp /= 10;
+        x5 = tmp % 10;
+        if (x5 >= 5)
+            flag = false;
+        tmp /= 10;
+        x6 = tmp % 10;
+        if (x6 >= 5)
+            flag = false;
+
+        ans += dp[getIndex(9 - x1, 9 - x2, 9 - x3, 9 - x4, 9 - x5, 9 - x6)];
+        
+        // 排除自己
+        if (flag)
+            ans --;
+    }
+
+    cout << ans / 2 << endl; 
 
     return 0;
 }

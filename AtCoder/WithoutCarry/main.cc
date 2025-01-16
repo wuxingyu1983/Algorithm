bb3028bc81ad43b681ab76552e2c6b49c5a1790c
LeoNg83 <85401996@qq.com>
2025-01-16 14:16:07 +0000

    init

--- AtCoder/WithoutCarry/main.cc
+++ AtCoder/WithoutCarry/main.cc
@@ -23,52 +23,11 @@
 
 using namespace std;
 
-#define MAX_A   1000010
+int dp[1000010];
 
-int threshold[] = {10, 100, 1000, 10000, 100000, 1000000};
-
-vector<int> d;
-vector<int> b;
-
-// segment tree functions
-void update(int l, int r, int c, int s, int t, int p)
+inline int getIndex(int x1, int x2, int x3, int x4, int x5, int x6)
 {
-    if (l <= s && t <= r)
-    {
-        d[p] += (t - s + 1) * c, b[p] += c;
-        return;
-    }
-    int m = s + ((t - s) >> 1);
-    if (b[p] && s != t)
-    {
-        d[p * 2] += b[p] * (m - s + 1), d[p * 2 + 1] += b[p] * (t - m);
-        b[p * 2] += b[p], b[p * 2 + 1] += b[p];
-        b[p] = 0;
-    }
-    if (l <= m)
-        update(l, r, c, s, m, p * 2);
-    if (r > m)
-        update(l, r, c, m + 1, t, p * 2 + 1);
-    d[p] = d[p * 2] + d[p * 2 + 1];
-}
-
-int getsum(int l, int r, int s, int t, int p)
-{
-    if (l <= s && t <= r)
-        return d[p];
-    int m = s + ((t - s) >> 1);
-    if (b[p])
-    {
-        d[p * 2] += b[p] * (m - s + 1), d[p * 2 + 1] += b[p] * (t - m);
-        b[p * 2] += b[p], b[p * 2 + 1] += b[p];
-        b[p] = 0;
-    }
-    int sum = 0;
-    if (l <= m)
-        sum = getsum(l, r, s, m, p * 2);
-    if (r > m)
-        sum += getsum(l, r, m + 1, t, p * 2 + 1);
-    return sum;
+    return x1 + 10 * x2 + 100 * x3 + 1000 * x4 + 10000 * x5 + 100000 * x6;
 }
 
 int main()
@@ -82,32 +41,72 @@ int main()
         int tmp;
         cin >> tmp;
         a.push_back(tmp);
+
+        dp[tmp] ++;
     }
 
-    // init segment tree
-    d.clear();
-    d.resize(4 * MAX_A + 4, 0);
-    b.clear();
-    b.resize(4 * MAX_A + 4, 0);
+    for (size_t x1 = 1; x1 <= 9; x1++)
+        for (size_t x2 = 0; x2 <= 9; x2++)
+            for (size_t x3 = 0; x3 <= 9; x3++)
+                for (size_t x4 = 0; x4 <= 9; x4++)
+                    for (size_t x5 = 0; x5 <= 9; x5++)
+                        for (size_t x6 = 0; x6 <= 9; x6++)
+                        {
+                            dp[getIndex(x1, x2, x3, x4, x5, x6)] += dp[getIndex(x1 - 1, x2, x3, x4, x5, x6)];
+                        }
+    
+    for (size_t x1 = 0; x1 <= 9; x1++)
+        for (size_t x2 = 1; x2 <= 9; x2++)
+            for (size_t x3 = 0; x3 <= 9; x3++)
+                for (size_t x4 = 0; x4 <= 9; x4++)
+                    for (size_t x5 = 0; x5 <= 9; x5++)
+                        for (size_t x6 = 0; x6 <= 9; x6++)
+                        {
+                            dp[getIndex(x1, x2, x3, x4, x5, x6)] += dp[getIndex(x1, x2 - 1, x3, x4, x5, x6)];
+                        }
+ 
+    for (size_t x1 = 0; x1 <= 9; x1++)
+        for (size_t x2 = 0; x2 <= 9; x2++)
+            for (size_t x3 = 1; x3 <= 9; x3++)
+                for (size_t x4 = 0; x4 <= 9; x4++)
+                    for (size_t x5 = 0; x5 <= 9; x5++)
+                        for (size_t x6 = 0; x6 <= 9; x6++)
+                        {
+                            dp[getIndex(x1, x2, x3, x4, x5, x6)] += dp[getIndex(x1, x2, x3 - 1, x4, x5, x6)];
+                        }
+ 
+    for (size_t x1 = 0; x1 <= 9; x1++)
+        for (size_t x2 = 0; x2 <= 9; x2++)
+            for (size_t x3 = 0; x3 <= 9; x3++)
+                for (size_t x4 = 1; x4 <= 9; x4++)
+                    for (size_t x5 = 0; x5 <= 9; x5++)
+                        for (size_t x6 = 0; x6 <= 9; x6++)
+                        {
+                            dp[getIndex(x1, x2, x3, x4, x5, x6)] += dp[getIndex(x1, x2, x3, x4 - 1, x5, x6)];
+                        }
+ 
+    for (size_t x1 = 0; x1 <= 9; x1++)
+        for (size_t x2 = 0; x2 <= 9; x2++)
+            for (size_t x3 = 0; x3 <= 9; x3++)
+                for (size_t x4 = 0; x4 <= 9; x4++)
+                    for (size_t x5 = 1; x5 <= 9; x5++)
+                        for (size_t x6 = 0; x6 <= 9; x6++)
+                        {
+                            dp[getIndex(x1, x2, x3, x4, x5, x6)] += dp[getIndex(x1, x2, x3, x4, x5 - 1, x6)];
+                        }
+ 
+    for (size_t x1 = 0; x1 <= 9; x1++)
+        for (size_t x2 = 0; x2 <= 9; x2++)
+            for (size_t x3 = 0; x3 <= 9; x3++)
+                for (size_t x4 = 0; x4 <= 9; x4++)
+                    for (size_t x5 = 0; x5 <= 9; x5++)
+                        for (size_t x6 = 1; x6 <= 9; x6++)
+                        {
+                            dp[getIndex(x1, x2, x3, x4, x5, x6)] += dp[getIndex(x1, x2, x3, x4, x5, x6 - 1)];
+                        }
 
     long long ans = 0;
-
-    for (size_t i = 0; i < n; i++)
-    {
-        int tmp = a[i];
-
-        // get sum
-        for (size_t j = 0; j < 6; j++)
-        {
-            if (threshold[j] > tmp)
-            {
-                ans += getsum(threshold[j] - tmp, threshold[j], 0, 1000000, 1);
-            }
-        }
-
-        // update segment tree
-        update(tmp, tmp, 1, 0, 1000000, 1);
-    }
+    
 
     cout << ans << endl; 
 

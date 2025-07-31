5ecff42c4d8d3eaa0f4ccd4201580d1a4f73ff86
LeoNg83 <85401996@qq.com>
2025-07-31 10:50:51 +0800

    AC

--- LeetCode/3351/main.cc
+++ LeetCode/3351/main.cc
@@ -20,6 +20,43 @@ public:
     {
         int ret = 0;
 
+        const int maxN = 100005;
+        const int mod = 1000000007;
+
+        long long cnt[maxN];
+        long long sum[maxN];
+        memset(cnt, 0, sizeof(cnt));
+        memset(sum, 0, sizeof(sum));
+
+        long long total = 0;
+
+        for (size_t i = 0; i < nums.size(); i++)
+        {
+            long long num = nums[i];
+            long long tmpCnt = 1;
+            long long tmpSum = 0;
+
+            if (0 < num)
+            {
+                tmpCnt += cnt[num - 1];
+                tmpSum += sum[num - 1];
+            }
+            
+            tmpCnt += cnt[num + 1];
+            tmpSum += sum[num + 1];
+
+            cnt[num] += tmpCnt;
+            cnt[num] %= mod;
+
+            sum[num] += tmpSum + num * tmpCnt;
+            sum[num] %= mod;
+
+            total += tmpSum + num * tmpCnt;
+            total %= mod;
+        }
+
+        ret = total;
+
         return ret;
     }
 };

1dbf0b3a1ade19ccaec3a765eb6b0a450453a4f7
LeoNg83 <85401996@qq.com>
2025-08-01 13:25:00 +0800

    init

--- LeetCode/3260/main.cc
+++ LeetCode/3260/main.cc
@@ -18,11 +18,49 @@ class Solution
 public:
     string largestPalindrome(int n, int k)
     {
-        string strRet;
+        string str;
 
+        const int maxN = 100002;
         
+        int rmds[maxN];
+        memset(rmds, 0, sizeof(rmds));
 
-        return strRet;
+        int tmp = 1;
+        for (size_t i = 0; i < n; i++)
+        {
+            rmds[i] = tmp % k;
+            tmp = (10 * tmp) % k;
+        }
+
+        int dp[maxN][9];
+        memset(dp, 0, sizeof(dp));
+
+        int idx;
+        if (n % 2)
+        {
+            idx = (n - 1) / 2;
+
+            for (size_t i = 1; i < 10; i++)
+            {
+                int rd = i * rmds[idx] % k;
+                dp[idx][rd] = i;
+            }
+            
+
+        }
+        else
+        {
+            idx = n / 2;
+
+            for (size_t i = 1; i < 10; i++)
+            {
+                int rd = i * (rmds[i] * rmds[i - 1]) % k;
+                dp[idx][rd] = i;
+            }
+            
+        }
+
+        return str;
     }
 };
 

06e4c4656b47406e692641875a6aa1bc60b70595
LeoNg83 <85401996@qq.com>
2025-08-01 12:49:03 +0800

    init

---
+++ LeetCode/3260/main.cc
@@ -0,0 +1,39 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+class Solution
+{
+public:
+    string largestPalindrome(int n, int k)
+    {
+        string strRet;
+
+        
+
+        return strRet;
+    }
+};
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    Solution s;
+    
+    cout << s.largestPalindrome(n, k) << endl;
+
+    return 0;
+}

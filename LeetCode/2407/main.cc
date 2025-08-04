1a0e2fdfa55c8142ea2d4d925cbfbeee8f687b28
LeoNg83 <85401996@qq.com>
2025-08-05 00:58:55 +0800

    init

---
+++ LeetCode/2407/main.cc
@@ -0,0 +1,48 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int lengthOfLIS(vector<int> &nums, int k)
+    {
+        int ret = 0;
+
+        return ret;
+    }
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> nums;
+    for (size_t i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+
+        nums.push_back(num);
+    }
+
+    int k;
+    cin >> k;
+
+    Solution s;
+    cout << s.lengthOfLIS(nums, k) << endl;
+    
+    return 0;
+}

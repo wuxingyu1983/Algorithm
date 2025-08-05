ae40c829e1a7e76b319661dd18b3a452893de6bd
LeoNg83 <85401996@qq.com>
2025-08-05 23:51:33 +0800

    init

--- LeetCode/1977/main.cc
+++ LeetCode/1977/main.cc
@@ -20,13 +20,44 @@ public:
     {
         int ret = 0;
 
-        const int maxLen = 3500;
+        const int maxLen = 3502;
         const int mod = 1000000007;
 
-        int dp[maxLen][maxLen];
-        memset(dp, 0, sizeof(dp));
+        if ('0' != num.at(0))
+        {
+            int dp[maxLen][maxLen]; // dp[idx][len];    string 中的 idx 位置为终点，长度 <= len 的总数和
+            memset(dp, 0, sizeof(dp));
 
+            dp[1][1] = 1;
 
+            for (size_t idx = 2; idx <= num.length(); idx++)
+            {
+                for (size_t l = 1; l <= idx; l++)
+                {
+                    // 比较以 idx 和 idx -l 为终点，长度为l的两个字符串
+                    string subStr1 = num.substr(idx - l, l);
+
+                    if ('0' != subStr1.at(0))
+                    {
+                        string subStr2 = num.substr(idx - l - l, l);
+                        if ('0' != subStr2.at(0) && subStr2.compare(subStr1) <= 0)
+                        {
+                            // 有效
+                            dp[idx][l] = dp[idx - l][l];
+                        }
+                        else
+                        {
+                            dp[idx][l] = dp[idx - l][l - 1];
+                        }
+                    }
+
+                    dp[idx][l] += dp[idx][l - 1];
+                    dp[idx][l] %= mod;
+                }
+            }
+
+            ret = dp[num.length()][num.length()];
+        }
 
         return ret;
     }

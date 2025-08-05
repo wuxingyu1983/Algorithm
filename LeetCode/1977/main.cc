e3b91f108fb1adbafa1aa41e7a174999af75971b
LeoNg83 <85401996@qq.com>
2025-08-06 00:46:16 +0800

    init

--- LeetCode/1977/main.cc
+++ LeetCode/1977/main.cc
@@ -23,23 +23,31 @@ public:
         const int maxLen = 3502;
         const int mod = 1000000007;
 
+        static int dp[maxLen][maxLen]; // dp[idx][len];    string 中的 idx 位置为终点，长度 <= len 的总数和
+
         if ('0' != num.at(0))
         {
-            int dp[maxLen][maxLen]; // dp[idx][len];    string 中的 idx 位置为终点，长度 <= len 的总数和
             memset(dp, 0, sizeof(dp));
 
-            dp[1][1] = 1;
-
-            for (size_t idx = 2; idx <= num.length(); idx++)
+            for (int l = 0; l <= num.length(); l++)
             {
-                for (size_t l = 1; l <= idx; l++)
+                dp[0][l] = 1;
+            }
+
+            for (int idx = 1; idx <= num.length(); idx++)
+            {
+                for (int l = 1; l <= idx; l++)
                 {
                     // 比较以 idx 和 idx -l 为终点，长度为l的两个字符串
                     string subStr1 = num.substr(idx - l, l);
 
                     if ('0' != subStr1.at(0))
                     {
-                        string subStr2 = num.substr(idx - l - l, l);
+                        string subStr2 = "0";
+                        if (0 <= idx - l - l)
+                        {
+                            subStr2 = num.substr(idx - l - l, l);
+                        }
                         if ('0' != subStr2.at(0) && subStr2.compare(subStr1) <= 0)
                         {
                             // 有效
@@ -53,6 +61,11 @@ public:
 
                     dp[idx][l] += dp[idx][l - 1];
                     dp[idx][l] %= mod;
+                }
+
+                for (int l = idx + 1; l <= num.length(); l++)
+                {
+                    dp[idx][l] += dp[idx][l - 1];
                 }
             }
 

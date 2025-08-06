2a9ea6e233bb461b6e21d86ea4479f459e458651
LeoNg83 <85401996@qq.com>
2025-08-06 16:32:41 +0800

    AC

--- LeetCode/1977/main.cc
+++ LeetCode/1977/main.cc
@@ -39,18 +39,23 @@ public:
                 for (int l = 1; l <= idx; l++)
                 {
                     // 比较以 idx 和 idx -l 为终点，长度为l的两个字符串
-                    string subStr1 = num.substr(idx - l, l);
+                    //                    string subStr1 = num.substr(idx - l, l);
+                    char subCh1 = num.at(idx - l);
 
-                    if ('0' != subStr1.at(0))
+                    if ('0' != subCh1)
                     {
-                        string subStr2 = "0";
+                        //                        string subStr2 = "0";
+                        char subCh2 = '0';
                         if (0 <= idx - l - l)
                         {
-                            subStr2 = num.substr(idx - l - l, l);
+                            //                            subStr2 = num.substr(idx - l - l, l);
+                            subCh2 = num.at(idx - 2 * l);
                         }
-                        if ('0' != subStr2.at(0) && subStr2.compare(subStr1) <= 0)
+                        //                        if ('0' != subStr2.at(0) && subStr2.compare(subStr1) <= 0)
+                        //                        if ('0' != subCh2 && compare(num, idx - 2 * l, idx - l, l) <= 0)
+                        if ('0' != subCh2 && num.compare(idx - 2 * l, l, num, idx - l, l) <= 0)
                         {
-                            // 有效
+                            // str2 <= str1, 有效
                             dp[idx][l] = dp[idx - l][l];
                         }
                         else
@@ -65,11 +70,37 @@ public:
 
                 for (int l = idx + 1; l <= num.length(); l++)
                 {
-                    dp[idx][l] += dp[idx][l - 1];
+                    dp[idx][l] = dp[idx][l - 1];
                 }
             }
 
             ret = dp[num.length()][num.length()];
+        }
+
+        return ret;
+    }
+
+private:
+    int compare(string &str, int idx1, int idx2, int len)
+    {
+        int ret = 0;
+
+        char ch1, ch2;
+        for (size_t i = 0; i < len; i++)
+        {
+            ch1 = str.at(idx1 + 1);
+            ch2 = str.at(idx2 + i);
+
+            if (ch1 < ch2)
+            {
+                ret = -1;
+                break;
+            }
+            else if (ch1 > ch2)
+            {
+                ret = 1;
+                break;
+            }
         }
 
         return ret;

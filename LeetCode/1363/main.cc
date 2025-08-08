dfdb93fee040b177196384321258ba8da6838912
LeoNg83 <85401996@qq.com>
2025-08-08 10:01:55 +0800

    init

--- LeetCode/1363/main.cc
+++ LeetCode/1363/main.cc
@@ -18,7 +18,7 @@ class Solution
 public:
     string largestMultipleOfThree(vector<int> &digits)
     {
-        string strRet;
+        string strRet = "";
 
         // 降序
         std::sort(digits.begin(), digits.end(), [](int a, int b)
@@ -34,7 +34,41 @@ public:
             cnt[digits[i]] ++;
         }
         
-        
+        int dp[10][3];
+        memset(dp, -1, sizeof(dp));     //dp[digit][mod 3]  最大数字为 digit ，总和 mod 3 的最大长度
+
+        dp[0][0] = cnt[0];
+
+        for (size_t num = 1; num < 10; num++)
+        {
+            int n = cnt[num];
+            for (int i = 0; i < 3 && n - i >= 0; i++)
+            {
+                for (size_t mod = 0; mod < 3; mod++)
+                {
+                    if (0 <= dp[num - 1][mod])
+                    {
+                        int newMod = (mod + (n - i) * num) % 3;
+                        if (dp[num][newMod] < dp[num - 1][mod])
+                        {
+                            dp[num][newMod] = dp[num - 1][mod];
+                        }
+                    }
+                }
+            }
+        }
+       
+        if (0 <= dp[9][0])
+        {
+            if (cnt[0] == dp[9][0])
+            {
+                strRet = "0";
+            }
+            else
+            {
+
+            }
+        }
 
         return strRet;
     }

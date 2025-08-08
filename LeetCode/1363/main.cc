4eeb74ab0937995e0971e33c6768fe4d24ab3c34
LeoNg83 <85401996@qq.com>
2025-08-08 12:57:38 +0800

    init

--- LeetCode/1363/main.cc
+++ LeetCode/1363/main.cc
@@ -20,22 +20,16 @@ public:
     {
         string strRet = "";
 
-        // 降序
-        std::sort(digits.begin(), digits.end(), [](int a, int b)
-                  {
-                      return a > b;
-                  });
-
         int cnt[10];
         memset(cnt, 0, sizeof(cnt));
 
         for (size_t i = 0; i < digits.size(); i++)
         {
-            cnt[digits[i]] ++;
+            cnt[digits[i]]++;
         }
-        
+
         int dp[10][3];
-        memset(dp, -1, sizeof(dp));     //dp[digit][mod 3]  最大数字为 digit ，总和 mod 3 的最大长度
+        memset(dp, -1, sizeof(dp)); // dp[digit][mod 3]  最大数字为 digit ，总和 mod 3 的最大长度
 
         dp[0][0] = cnt[0];
 
@@ -49,18 +43,18 @@ public:
                     if (0 <= dp[num - 1][mod])
                     {
                         int newMod = (mod + (n - i) * num) % 3;
-                        if (dp[num][newMod] < dp[num - 1][mod])
+                        if (dp[num][newMod] < dp[num - 1][mod] + n - i)
                         {
-                            dp[num][newMod] = dp[num - 1][mod];
+                            dp[num][newMod] = dp[num - 1][mod] + n - i;
                         }
                     }
                 }
             }
         }
-       
+
         if (0 <= dp[9][0])
         {
-            if (cnt[0] == dp[9][0])
+            if (cnt[0] == dp[9][0] && 0 < cnt[0])
             {
                 strRet = "0";
             }
@@ -77,7 +71,7 @@ public:
                         {
                             if (0 < (n - i))
                             {
-                                strRet.append('0' + num, n - i);
+                                strRet.append(n - i, '0' + num);
                             }
 
                             mod = newMod;
@@ -85,6 +79,11 @@ public:
                             break;
                         }
                     }
+                }
+
+                if (0 < cnt[0])
+                {
+                    strRet.append(cnt[0], '0');
                 }
             }
         }
@@ -109,6 +108,6 @@ int main()
 
     Solution s;
     cout << s.largestMultipleOfThree(digits) << endl;
-    
+
     return 0;
 }

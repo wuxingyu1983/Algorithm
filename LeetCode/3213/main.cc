50d94d9a8e51c7ceffff02f68c9915803e5f9c4d
LeoNg83 <85401996@qq.com>
2025-08-10 11:31:37 +0800

    init

---
+++ LeetCode/3213/main.cc
@@ -0,0 +1,57 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+#include <map>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int minimumCost(string target, vector<string> &words, vector<int> &costs)
+    {
+        int ret = 1;
+
+        return ret;
+    }
+};
+
+int main()
+{
+    string target;
+    cin >> target;
+
+    int n;
+    cin >> n;
+
+    vector<string> words;
+    vector<int> costs;
+
+    for (size_t i = 0; i < n; i++)
+    {
+        string word;
+        cin >> word;
+        words.push_back(word);
+    }
+    
+    for (size_t i = 0; i < n; i++)
+    {
+        int cost;
+        cin >> cost;
+        costs.push_back(cost);
+    }
+    
+    Solution s;
+    cout << s.minimumCost(target, words, costs);
+
+    return 0;
+}

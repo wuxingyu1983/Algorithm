2de2ad2022287279a6ab7a19b3133643b0c21ce5
LeoNg83 <85401996@qq.com>
2025-08-13 00:35:01 +0800

    AC

--- LeetCode/3448/main.cc
+++ LeetCode/3448/main.cc
@@ -22,6 +22,43 @@ public:
     {
         long long ret = 0;
 
+        vector<vector<long long>> dp(s.length(), vector<long long>(81, 0));
+
+        int digit = s.at(0) - '0';
+        for (size_t num = 1; num <= 9; num++)
+        {
+            dp[0][(num - 1) * 9 + digit % num]++;
+        }
+
+        if (0 < digit)
+        {
+            ret += dp[0][(digit - 1) * 9];
+        }
+
+        for (size_t i = 1; i < s.length(); i++)
+        {
+            int digit = s.at(i) - '0';
+
+            for (size_t num = 1; num <= 9; num++)
+            {
+                dp[i][(num - 1) * 9 + digit % num]++;
+
+                for (size_t oldMod = 0; oldMod < num; oldMod++)
+                {
+                    if (0 < dp[i - 1][(num - 1) * 9 + oldMod])
+                    {
+                        int newMod = (oldMod * 10 + digit) % num;
+                        dp[i][(num - 1) * 9 + newMod] += dp[i - 1][(num - 1) * 9 + oldMod];
+                    }
+                }
+            }
+
+            if (0 < digit)
+            {
+                ret += dp[i][(digit - 1) * 9];
+            }
+        }
+
         return ret;
     }
 };

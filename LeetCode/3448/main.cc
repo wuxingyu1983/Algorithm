e48caf4bf4258292c425aed9fcc0343f47f8ea93
LeoNg83 <85401996@qq.com>
2025-08-12 22:47:43 +0800

    init

---
+++ LeetCode/3448/main.cc
@@ -0,0 +1,39 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <bitset>
+
+using namespace std;
+
+class Solution
+{
+public:
+    long long countSubstrings(string s)
+    {
+        long long ret = 0;
+
+        return ret;
+    }
+};
+
+int main()
+{
+    string str;
+    cin >> str;
+
+    Solution s;
+
+    cout << s.countSubstrings(str) << endl;
+
+    return 0;
+}

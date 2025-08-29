6b6a9a425c481ca333ca318a4ad60588f48bfac9
LeoNg83 <85401996@qq.com>
2025-08-29 11:21:21 +0800

    init

---
+++ LeetCode/2827/main.cc
@@ -0,0 +1,38 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <bitset>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int numberOfBeautifulIntegers(int low, int high, int k)
+    {
+        int ret = 0;
+
+        return ret;
+    }
+};
+
+int main()
+{
+    int low, high, k;
+    cin >> low >> high >> k;
+
+    Solution s;
+    cout << s.numberOfBeautifulIntegers(low, high, k);
+
+    return 0;
+}

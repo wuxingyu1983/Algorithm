0ade4cd16753a32df47661ed2c7c417bef37547f
LeoNg83 <85401996@qq.com>
2025-09-03 01:54:51 +0800

    init

---
+++ LeetCode/887/main.cc
@@ -0,0 +1,38 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <bitset>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int superEggDrop(int k, int n)
+    {
+        int ret = 0;
+
+        return ret;
+    }
+};
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    Solution s;
+    cout << s.superEggDrop(k, n) << endl;
+
+    return 0;
+}

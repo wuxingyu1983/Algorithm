7ed358999f4f19c84cc22a42f6af0e9fc7c87d16
LeoNg83 <85401996@qq.com>
2025-09-03 16:58:26 +0800

    78 / 121, time out

--- LeetCode/887/main.cc
+++ LeetCode/887/main.cc
@@ -23,20 +23,17 @@ public:
         int ret = 0;
 
         vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
-        vector<vector<int>> firstIdx(k + 1, vector<int>(n + 1, 0));
 
         // init
         {
             for (size_t floor = 1; floor <= n; floor++)
             {
                 dp[floor][1] = floor;
-                firstIdx[1][floor] = floor;
             }
 
             for (size_t iK = 2; iK <= k; iK++)
             {
                 dp[1][iK] = 1;
-                firstIdx[iK][1] = 1;
             }
         }
 
@@ -45,63 +42,16 @@ public:
             for (size_t iK = 2; iK <= k; iK++)
             {
                 dp[floor][iK] = -1;
-                for (size_t left = 1; left <= floor && firstIdx[iK - 1][left] && firstIdx[iK - 1][left] <= floor; left++)
+                
+                for (size_t i = 1; i <= floor; i++)
                 {
-                    int stp = firstIdx[iK - 1][left];
-
-                    int tmp = left;
-                    if (floor > stp + 1)
-                    {
-                        if (tmp < dp[floor - stp - 1][iK])
-                        {
-                            tmp = dp[floor - stp - 1][iK];
-                        }
-                    }
-
-                    if (floor > stp)
-                        tmp += 1;
-
-                    if (0 > dp[floor][iK] || dp[floor][iK] > tmp)
-                    {
-                        dp[floor][iK] = tmp;
-                    }
-
-                    if (0 == firstIdx[iK][dp[floor][iK]])
-                    {
-                        firstIdx[iK][dp[floor][iK]] = floor;
-                    }
-                }
-
-                {
-                    int mid = floor / 2;
-                    if (1 & floor)
-                    {
-                        mid += 1;
-                    }
-
-                    int tmp = 0;
-                    if (1 < mid)
-                    {
-                        tmp = dp[mid - 1][iK - 1];
-                    }
-
-                    if (tmp < dp[floor - mid][iK])
-                    {
-                        tmp = dp[floor - mid][iK];
-                    }
-
+                    int tmp = max(dp[i - 1][iK - 1], dp[floor - i][iK]);
                     tmp += 1;
 
                     if (0 > dp[floor][iK] || dp[floor][iK] > tmp)
-                    {
                         dp[floor][iK] = tmp;
-                    }
-
-                    if (0 == firstIdx[iK][dp[floor][iK]])
-                    {
-                        firstIdx[iK][dp[floor][iK]] = floor;
-                    }
                 }
+                
             }
         }
 

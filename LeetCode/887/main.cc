d8fd729b441647cf53d1be78b29be2f2d64eee37
LeoNg83 <85401996@qq.com>
2025-09-03 10:55:48 +0800

    init

--- LeetCode/887/main.cc
+++ LeetCode/887/main.cc
@@ -22,6 +22,55 @@ public:
     {
         int ret = 0;
 
+        vector<int> steps = {1, 3, 7, 15, 31, 63, 127, 255, 511, 1023, 2047, 4095, 8191};
+        vector< vector<int> > dp(n + 1, vector<int>(k + 1, 0));
+
+        // init
+        {
+            for (size_t floor = 1; floor <= n; floor++)
+            {
+                dp[floor][1] = floor;
+            }
+
+            for (size_t iK = 2; iK <= k; iK++)
+            {
+                dp[1][iK] = 1;
+            }
+        }
+
+        for (int floor = 2; floor <= n; floor++)
+        {
+            for (size_t iK = 2; iK <= k; iK++)
+            {
+                dp[floor][iK] = -1;
+                for (size_t i = 0; i < steps.size(); i++)
+                {
+                    int stp = steps[i];
+                    if (0 >= floor - stp)
+                        break;
+                    
+                    int tmp = dp[stp][iK];
+                    if (0 < floor - stp - 1)
+                    {
+                        if (tmp < dp[floor - stp - 1][iK - 1])
+                        {
+                            tmp = dp[floor - stp - 1][iK - 1];
+                        }
+                    }
+                    
+                    if (0 < floor - stp)
+                        tmp += 1;
+
+                    if (0 > dp[floor][iK] || dp[floor][iK] > tmp)
+                    {
+                        dp[floor][iK] = tmp;
+                    }
+                }
+            }
+        }
+
+        ret = dp[n][k];
+
         return ret;
     }
 };
@@ -29,7 +78,7 @@ public:
 int main()
 {
     int n, k;
-    cin >> n >> k;
+    cin >> k >> n;
 
     Solution s;
     cout << s.superEggDrop(k, n) << endl;

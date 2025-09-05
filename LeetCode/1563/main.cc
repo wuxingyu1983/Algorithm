88fab9bd1edcd331c079eefdec85e801077e16d2
LeoNg83 <85401996@qq.com>
2025-09-05 13:38:04 +0800

    AC

--- LeetCode/1563/main.cc
+++ LeetCode/1563/main.cc
@@ -22,6 +22,56 @@ public:
     {
         int ret = 0;
 
+        vector<int> sum(stoneValue.size());
+        sum[0] = stoneValue[0];
+        for (size_t i = 1; i < stoneValue.size(); i++)
+        {
+            sum[i] = sum[i - 1] + stoneValue[i];
+        }
+        
+        vector<vector<int>> dp(stoneValue.size(), vector<int>(stoneValue.size(), 0));
+
+        for (size_t stp = 1; stp < stoneValue.size(); stp++)
+        {
+            for (size_t x = 0; x < stoneValue.size() && x + stp < stoneValue.size(); x++)
+            {
+                for (size_t i = 0; i < stp; i++)
+                {
+                    // left : x -> x + i
+                    int leftSum = sum[x + i];
+                    if (0 < x)
+                    {
+                        leftSum -= sum[x - 1];
+                    }
+
+                    // right : x + i + 1 -> x + stp
+                    int rightSum = sum[x + stp] - sum[x + i];
+
+                    int tmp = 0;
+                    if (leftSum > rightSum)
+                    {
+                        tmp = rightSum + dp[x + i + 1][x + stp];
+                    }
+                    else if (leftSum < rightSum)
+                    {
+                        tmp = leftSum + dp[x][x + i];
+                    }
+                    else
+                    {
+                        // ==
+                        tmp = leftSum + max(dp[x][x + i], dp[x + i + 1][x + stp]);
+                    }
+
+                    if (tmp > dp[x][x + stp])
+                    {
+                        dp[x][x + stp] = tmp;
+                    }
+                }
+            }
+        }
+
+        ret = dp[0][stoneValue.size() - 1];
+
         return ret;
     }
 };

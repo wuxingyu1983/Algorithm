e47d5d5f1cd0ea33e2ce20f6833c83dcab1b9e07
LeoNg83 <85401996@qq.com>
2025-09-09 02:07:09 +0800

    init

--- LeetCode/3490/main.cc
+++ LeetCode/3490/main.cc
@@ -57,18 +57,33 @@ private:
                 // 0
                 ret += dfs(dp, strNum, 0, pos + 1, false);
                 // clear
-                
+                for (size_t sum = 1; sum < 82; sum++)
+                {
+                    dp[pos][sum].clear();
+                }
 
                 // 1 -> up - 1
                 for (size_t n = 1; n < up; n++)
                 {
-//                    dp[pos][n][0] ++;
+                    for (size_t mod = 1; mod < 82; mod++)
+                    {
+                        int key = (mod << 7) | (n % mod);
+                        dp[pos][n][key] ++;
+                    }
                 }
                 ret += dfs(dp, strNum, 1, pos + 1, false);
                 // clear
+                for (size_t sum = 1; sum < 82; sum++)
+                {
+                    dp[pos][sum].clear();
+                }
 
                 // up
-//                dp[pos][up][0] ++;
+                for (size_t mod = 1; mod < 82; mod++)
+                {
+                    int key = (mod << 7) | (up % mod);
+                    dp[pos][up][key] ++;
+                }
                 ret += dfs(dp, strNum, 1, pos + 1, true);
             }
             else
@@ -80,13 +95,38 @@ private:
                 }
 
                 // clear
+                for (size_t sum = 1; sum < 82; sum++)
+                {
+                    dp[pos][sum].clear();
+                }
                 
                 for (size_t n = 0; n < up; n++)
                 {
-                    
+                    for (size_t sum = 1; sum < 82; sum++)
+                    {
+                        if (0 < dp[pos - 1][sum].size())
+                        {
+                            for (unordered_map<int, int>::iterator it = dp[pos - 1][sum].begin(); it != dp[pos - 1][sum].end(); it++)
+                            {
+                                int oldMod = it->first & 127;
+                                int oldS = it->first >> 7;
+
+                                int newSum = sum + n;
+                                int newMod = (oldMod * n) % oldS;
+
+                                int newKey = (oldS << 7) + newMod;
+                                dp[pos][newSum][newKey] += it->second;
+                            }
+                        }
+                    }
                 }
                 
             }
+        }
+        else
+        {
+            // pos == strNum.length()
+
         }
 
         return ret;

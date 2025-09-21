4118819c3579486955597f96d356d8bf220153e0
LeoNg83 <85401996@qq.com>
2025-09-21 14:56:40 +0800

    init

--- HDU/6148/main.cc
+++ HDU/6148/main.cc
@@ -60,13 +60,13 @@ int main()
         {
             for (int m = 0; m <= n; m++)
             {
-                dp[idx][1][n] += dp[idx - 1][0][m];
-                dp[idx][1][n] += dp[idx - 1][1][m];
-                dp[idx][1][n] += dp[idx - 1][2][m];
+                dp[idx][2][n] += dp[idx - 1][0][m];
+                dp[idx][2][n] += dp[idx - 1][1][m];
+                dp[idx][2][n] += dp[idx - 1][2][m];
             }
 
-            dp[idx][1][n] %= mod;
-            sum[idx] += dp[idx][1][n];
+            dp[idx][2][n] %= mod;
+            sum[idx] += dp[idx][2][n];
             sum[idx] %= mod;
         }
 
@@ -79,9 +79,78 @@ int main()
         string strNum;
         cin >> strNum;
 
+        long long cnt = 0;
 
+        int type = -1;
+        int pre = -1;
+        int len = strNum.length();
+
+        if (1 == len)
+        {
+            cnt = strNum.at(0) - '0';
+        }
+        else
+        {
+            for (int idx = 0; idx < len - 1; idx++)
+            {
+                int up = strNum.at(idx) - '0';
+
+                if (0 == idx)
+                {
+                    // 0
+                    cnt += sum[len - 2];
+
+                    for (int n = 1; n < up; n++)
+                    {
+                        // type = 0
+                        cnt += dp[len - 2][0][n];
+                        cnt %= mod;
+
+                        // type = 1
+                        for (int m = n; m < 10; m++)
+                        {
+                            cnt += dp[len - 2][0][m];
+                            cnt += dp[len - 2][1][m];
+                            cnt %= mod;
+                        }
+
+                        // type = 2
+                        for (int m = 0; m <= n; m++)
+                        {
+                            cnt += dp[len - 2][0][m];
+                            cnt += dp[len - 2][1][m];
+                            cnt += dp[len - 2][2][m];
+                            cnt %= mod;
+                        }
+                    }
+                }
+                else if (1 == idx)
+                {
+                    for (int n = 0; n < up; n++)
+                    {
+                        if (n == pre)
+                        {
+                            // 3 种 type 都可以
+                        }
+                        else if (pre < n)
+                        {
+                            // type 0 和 1
+                        }
+                        else
+                        {
+                            // pre > n
+                            // 3 种 type 都可以
+                        }
+                    }
+                    
+                }
+                
+                pre = up;
+            }
+        }
+
+        cout << cnt << endl;
     }
-    
 
     return 0;
 }

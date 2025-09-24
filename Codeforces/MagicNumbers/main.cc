47194e4f7bdabb47a1545402db0aa92d656b4839
LeoNg83 <85401996@qq.com>
2025-09-25 01:15:23 +0800

    init

--- Codeforces/MagicNumbers/main.cc
+++ Codeforces/MagicNumbers/main.cc
@@ -55,5 +55,49 @@ int main()
         dp[len][d][d % m] = 1;
     }
 
+    for (int idx = len - 1; idx > 0; idx--)
+    {
+        if (idx & 1)
+        {
+            // odd, 奇数
+            for (int iM = 0; iM < m; iM++)
+            {
+                if (dp[idx + 1][d][iM])
+                {
+                    for (int n = 0; n < 10; n++)
+                    {
+                        if (n != d)
+                        {
+                            int newM = (iM + mods[idx] * n) % m;
+                            dp[idx][n][newM] += dp[idx + 1][d][iM];
+                            dp[idx][n][newM] %= mod;
+                        }
+                    }
+                }
+            }
+            
+        }
+        else
+        {
+            // even, 偶数
+            for (int n = 0; n < 10; n++)
+            {
+                if (n != d)
+                {
+                    for (int iM = 0; iM < m; iM++)
+                    {
+                        if (dp[idx + 1][n][iM])
+                        {
+                            int newM = (iM + mods[idx] * d) % m;
+                            dp[idx][d][newM] += dp[idx + 1][n][iM];
+                            dp[idx][d][newM] %= mod;
+                        }
+                    }
+                }
+            }
+            
+        }
+    }
+
     return 0;
 }

95e180da93e59ad0dab9558a6c35cf7743d0f13d
LeoNg83 <85401996@qq.com>
2025-09-26 03:10:25 +0800

    init

--- Codeforces/ClassyNumbers/main.cc
+++ Codeforces/ClassyNumbers/main.cc
@@ -17,7 +17,10 @@
 
 using namespace std;
 
-long long func(long long n)
+long long dp[19][10][4];
+long long sums[19];
+
+long long func(long long num)
 {
     long long ret = 0;
 
@@ -28,6 +31,55 @@ int main()
 {
     int t;
     cin >> t;
+
+    // init
+    {
+        int idx = 0;
+        for (int n = 0; n < 10; n++)
+        {
+            if (0 == n)
+            {
+                dp[idx][n][0] = 1;
+            }
+            else
+            {
+                dp[idx][n][1] = 1;
+            }
+        }
+
+        sums[idx] = 10;
+
+        for (idx = 0; idx <= 19; idx++)
+        {
+            for (int n = 0; n < 10; n++)
+            {
+                if (0 == n)
+                {
+                    for (int cnt = 0; cnt < 4; cnt++)
+                    {
+                        for (int m = 0; m < 10; m++)
+                        {
+                            dp[idx][n][cnt] += dp[idx - 1][m][cnt];
+                        }
+                    }
+                }
+                else
+                {
+                    for (int cnt = 1; cnt < 4; cnt++)
+                    {
+                        for (int m = 0; m < 10; m++)
+                        {
+                            dp[idx][n][cnt] += dp[idx - 1][m][cnt - 1];
+                        }
+
+                        sums[idx] += dp[idx][n][cnt];
+                    }
+                }
+            }
+
+            sums[idx] += sums[idx - 1];
+        }
+    }
 
     for (size_t iT = 0; iT < t; iT++)
     {

32d2aa8ef9dcaed17694a3b8b80a0c71703998e2
LeoNg83 <85401996@qq.com>
2025-09-26 03:49:47 +0800

    init

--- Codeforces/ClassyNumbers/main.cc
+++ Codeforces/ClassyNumbers/main.cc
@@ -24,6 +24,60 @@ long long func(long long num)
 {
     long long ret = 0;
 
+    string strNum = to_string(num);
+    reverse(strNum.begin(), strNum.end());
+
+    int nonZero = 0;
+    for (int idx = strNum.length() - 1; idx >= 0; idx--)
+    {
+        int up = strNum.at(idx) - '0';
+        if (idx == strNum.length() - 1)
+        {
+            if (1 <= idx)
+            {
+                ret += sums[idx - 1];
+            }
+            else
+            {
+                ret += 1;
+                up ++;
+            }
+
+            for (int n = 1; n < up; n++)
+            {
+                ret += dp[idx][n][1];
+                ret += dp[idx][n][2];
+                ret += dp[idx][n][3];
+            }
+
+            nonZero = 1;
+        }
+        else
+        {
+            if (0 == idx)
+            {
+                up ++;
+            }
+
+            for (int n = 0; n < up; n++)
+            {
+                for (int cnt = 0; cnt <= 3 - nonZero; cnt++)
+                {
+                    ret += dp[idx][n][cnt];
+                }
+            }
+
+            if (0 < up)
+            {
+                nonZero ++;
+                if (3 <= nonZero)
+                {
+                    break;
+                }
+            }
+        }
+    }
+
     return ret;
 }
 

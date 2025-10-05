39daaf604543575d679d84f2d042e85500279c95
LeoNg83 <85401996@qq.com>
2025-10-06 03:13:18 +0800

    init

--- Codeforces/BeautifulNumbers/main.cc
+++ Codeforces/BeautifulNumbers/main.cc
@@ -17,8 +17,11 @@
 
 using namespace std;
 
+vector<int> subsets[512];
 int lcm[512];
 int num[48] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 15, 18, 20, 21, 24, 28, 30, 35, 36, 40, 42, 45, 56, 60, 63, 70, 72, 84, 90, 105, 120, 126, 140, 168, 180, 210, 252, 280, 315, 360, 420, 504, 630, 840, 1260, 2520};
+
+long long dp[19][512][2520];
 
 int gcd(int a, int b)
 {
@@ -28,34 +31,75 @@ int gcd(int a, int b)
 		return gcd(b, a % b);
 }
 
-int main()
+void init()
 {
-    // init
+    for (int n = 1; n < 512; n++)
     {
-        for (int n = 1; n < 512; n++)
+        int l = 1;
+        for (int idx = 0; idx < 10; idx++)
         {
-            int l = 1;
-            for (int idx = 0; idx < 10; idx++)
+            if (n & (1 << idx))
             {
-                if (n & (1 << idx))
-                {
-                    l = l * (idx + 1) / gcd(l, idx + 1);
-                }
+                l = l * (idx + 1) / gcd(l, idx + 1);
             }
-            
-            lcm[n] = l;
+        }
+
+        lcm[n] = l;
+
+        for (int m = 1; m <= n; m++)
+        {
+            if (m == (m & n))
+            {
+                subsets[n].push_back(m);
+            }
         }
     }
+}
+
+void initDP(int st)
+{
+
+}
+
+long long func(long long num, int st)
+{
+    long long ret = 0;
+
+    return ret;
+}
+
+int main()
+{
+    init();
 
     int t;
     cin >> t;
+
+    vector<long long> ls, rs;
 
     for (size_t i = 0; i < t; i++)
     {
         long long l, r;
         cin >> l >> r;
 
-        
+        ls.push_back(l);
+        rs.push_back(r);
+    }
+
+    vector<long long> cnts(t, 0);
+    for (int st = 1; st < 512; st++)
+    {
+        initDP(st);
+
+        for (size_t i = 0; i < t; i++)
+        {
+            cnts[i] += func(rs[i], st) - func(ls[i] - 1, st);
+        }
+    }
+
+    for (size_t i = 0; i < t; i++)
+    {
+        cout << cnts[i] << endl;
     }
 
     return 0;

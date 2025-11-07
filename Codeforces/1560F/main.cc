9e82af6b90fab8a78dca74ef88fa9dc9546dbd3e
LeoNg83 <85401996@qq.com>
2025-11-08 00:25:26 +0800

    init

---
+++ Codeforces/1560F/main.cc
@@ -0,0 +1,41 @@
+// https://codeforces.com/problemset/problem/1560/F1
+// https://codeforces.com/problemset/problem/1560/F2
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <bitset>
+
+using namespace std;
+
+#pragma GCC target("popcnt")
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    for (size_t i = 0; i < t; i++)
+    {
+        int n, k;
+        cin >> n >> k;
+
+
+    }
+
+    return 0;
+}

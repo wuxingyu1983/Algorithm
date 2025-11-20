52c4bbfc8340d2b85c638a00707618966cae9cf0
LeoNg83 <85401996@qq.com>
2025-11-21 01:50:31 +0800

    init

---
+++ Codeforces/914C/main.cc
@@ -0,0 +1,51 @@
+// https://codeforces.com/problemset/problem/914/C
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <bitset>
+
+using namespace std;
+
+#pragma GCC target("popcnt")
+
+const int mod = 1000000007;
+
+int C[1001][1001];
+int ops[1001];
+int dp[1001][1001];
+
+int main()
+{
+    string strN;
+    int k;
+
+    cin >> strN;
+    cin >> k;
+
+    // init
+    {
+        for (size_t i = 2; i <= 1000; i++)
+        {
+            int stp = 0;
+            int newI = 0;
+
+
+        }
+        
+
+    }
+
+    return 0;
+}

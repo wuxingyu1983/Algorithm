5c4f1a497a626646eaec8eb0df15e2a58c2e3ac3
LeoNg83 <85401996@qq.com>
2025-11-22 16:36:01 +0800

    init

--- Codeforces/914C/main.cc
+++ Codeforces/914C/main.cc
@@ -23,7 +23,7 @@ using namespace std;
 const int mod = 1000000007;
 
 int C[1001][1001];
-int ops[1001];
+int ops[1001];          // ops[num], num 到 1 需要的 op 数
 int dp[1001][1001];
 
 int main()
@@ -38,13 +38,24 @@ int main()
     {
         for (size_t i = 2; i <= 1000; i++)
         {
-            int stp = 0;
-            int newI = 0;
-
-
+            ops[i] = 1 + ops[__builtin_popcount(i)];
         }
         
-
+        for (size_t n = 1; n <= 1000; n++)
+        {
+            for (size_t m = 0; m <= n; m++)
+            {
+                if (0 == m || m == n)
+                {
+                    C[n][m] = 1;
+                }
+                else
+                {
+                    C[n][m] = C[n - 1][m] + C[n - 1][m-1];
+                    C[n][m] %= mod;
+                }
+            }
+        }
     }
 
     return 0;

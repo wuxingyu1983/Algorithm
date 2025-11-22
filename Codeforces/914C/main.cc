2b2f087ff0e8bc5e1c08f3cef947fb39186831ee
LeoNg83 <85401996@qq.com>
2025-11-22 20:39:59 +0800

    wrong on 51

--- Codeforces/914C/main.cc
+++ Codeforces/914C/main.cc
@@ -23,8 +23,8 @@ using namespace std;
 const int mod = 1000000007;
 
 int C[1001][1001];
-int ops[1001];          // ops[num], num 到 1 需要的 op 数
-int dp[1001][1001];
+int ops[1001];
+long long dp[1001];
 
 int main()
 {
@@ -36,11 +36,12 @@ int main()
 
     // init
     {
+        ops[1] = 1;
         for (size_t i = 2; i <= 1000; i++)
         {
             ops[i] = 1 + ops[__builtin_popcount(i)];
         }
-        
+
         for (size_t n = 1; n <= 1000; n++)
         {
             for (size_t m = 0; m <= n; m++)
@@ -51,12 +52,66 @@ int main()
                 }
                 else
                 {
-                    C[n][m] = C[n - 1][m] + C[n - 1][m-1];
+                    C[n][m] = C[n - 1][m] + C[n - 1][m - 1];
                     C[n][m] %= mod;
                 }
             }
         }
     }
+
+    int preOnes = 0;
+    for (size_t pos = 0; pos < strN.length(); pos++)
+    {
+        int num = strN.at(pos) - '0';
+
+        if (0 < num)
+        {
+            int remain = strN.length() - 1 - pos;
+
+            if (0 < remain)
+            {
+                for (size_t i = 0; i <= remain; i++)
+                {
+                    dp[preOnes + i] += C[remain][i];
+                    dp[preOnes + i] %= mod;
+                }
+            }
+        }
+
+        preOnes += num;
+    }
+
+    dp[preOnes] += 1;
+    dp[preOnes] %= mod;
+
+    int ret = 0;
+
+    if (0 == k)
+    {
+        if (0 < preOnes)
+        {
+            ret = 1;
+        }
+    }
+    else
+    {
+        for (size_t i = 0; i <= 1000; i++)
+        {
+            if (k == ops[i])
+            {
+                ret += dp[i];
+                ret %= mod;
+            }
+        }
+
+        if (1 == k)
+        {
+            ret += mod - 1;
+            ret %= mod;
+        }
+    }
+
+    cout << ret << endl;
 
     return 0;
 }

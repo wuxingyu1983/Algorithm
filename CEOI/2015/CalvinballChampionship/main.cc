5da17b11f8f062c12ba678e2730ff30fbf85485d
LeoNg83 <85401996@qq.com>
2025-11-25 14:58:17 +0800

    init

---
+++ CEOI/2015/CalvinballChampionship/main.cc
@@ -0,0 +1,42 @@
+// https://www.luogu.com.cn/problem/P4798
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <bitset>
+
+using namespace std;
+
+const int mod = 1000007;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> tms;
+    for (size_t i = 0; i < n; i++)
+    {
+        int t;
+        cin >> t;
+
+        tms.push_back(t);
+    }
+   
+    int ans = 0;
+
+    cout << ans << endl;
+
+    return 0;
+}

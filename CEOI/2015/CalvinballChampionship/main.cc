7a42222de1bffa37e601a218dbf0ecb77467b636
LeoNg83 <85401996@qq.com>
2025-11-30 02:04:21 +0800

    AC

--- CEOI/2015/CalvinballChampionship/main.cc
+++ CEOI/2015/CalvinballChampionship/main.cc
@@ -18,10 +18,11 @@
 
 using namespace std;
 
-const int maxN = 10001;
+const int maxN = 10002;
 const int mod = 1000007;
 
 int maxT[maxN];
+long long dp[maxN];
 
 int main()
 {
@@ -53,7 +54,36 @@ int main()
         }
     }
    
-    int ans = 0;
+    long long ans = 0;
+
+    {
+        int i = n - 1;
+
+        for (size_t j = 1; j <= n; j++)
+        {
+            dp[j] = 1;
+        }
+
+        int num = tms[i];
+        ans += num;
+    }
+
+    for (int i = n - 2; i >= 0; i--)
+    {
+        int num = tms[i];
+
+        for (size_t j = 1; j <= n; j++)
+        {
+            dp[j] = j * dp[j] + dp[j + 1];
+            dp[j] %= mod;
+        }
+
+        if (num > 1)
+        {
+            ans += (num - 1) * dp[maxT[i - 1]];
+            ans %= mod;
+        }
+    }
 
     cout << ans << endl;
 

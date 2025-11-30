1ec9a0f3ee8bd66309d6a2ea9541a00b18bc5578
LeoNg83 <85401996@qq.com>
2025-11-30 15:44:19 +0800

    init

---
+++ Luogu/P3281/main.cc
@@ -0,0 +1,32 @@
+// https://www.luogu.com.cn/problem/P3281
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <bitset>
+
+using namespace std;
+
+const int mod = 20130427;
+vector<long long> ls, rs;
+
+int main()
+{
+    int b;
+    cin >> b;
+
+
+
+    return 0;
+}

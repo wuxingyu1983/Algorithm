a139599c40c38342e6d6a6d5183ed0c27986c0d9
LeoNg83 <85401996@qq.com>
2025-12-13 16:18:24 +0800

    AC

--- Toph/NooraNumber/main.cc
+++ Toph/NooraNumber/main.cc
@@ -22,10 +22,60 @@ using namespace std;
 
 int flag[1024];
 long long dp[20][1024];
+long long sum[20];
 
 long long func(string strNum)
 {
     long long ret = 0;
+    int len = strNum.length();
+    int pre = 0;
+
+    for (size_t pos = 0; pos < len; pos++)
+    {
+        int d = strNum.at(pos) - '0';
+        int down = 0;
+        int up = d - 1;
+        if (0 == pos)
+        {
+            down = 1;
+            ret += sum[len - pos - 2];
+        }
+
+        if (len - 1 == pos)
+        {
+            up = d;
+
+            for (size_t n = down; n <= up; n++)
+            {
+                if (flag[pre | (1 << n)])
+                {
+                    ret ++;
+                }
+            }
+        }
+        else
+        {
+            if (0 < d)
+            {
+                for (size_t i = 1; i < 1024; i++)
+                {
+                    if (dp[len - pos - 2][i])
+                    {
+                        for (size_t n = down; n <= up; n++)
+                        {
+                            if (flag[pre | i | (1 << n)])
+                            {
+                                ret += dp[len - pos - 2][i];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        pre |= 1 << d;
+    }
+    
 
     return ret;
 }
@@ -71,6 +121,10 @@ int main()
         for (size_t i = 0; i < 10; i++)
         {
             dp[pos][1 << i] = 1;
+            if (flag[1 << i])
+            {
+                sum[pos] ++;
+            }
         }
        
         for (; pos < 19; pos++)
@@ -82,9 +136,19 @@ int main()
                     for (size_t i = 0; i < 10; i++)
                     {
                         dp[pos + 1][n | (1 << i)] += dp[pos][n];
+
+                        if (0 < i && flag[n | (1 << i)])
+                        {
+                            sum[pos + 1] += dp[pos][n];
+                        }
                     }
                 }
             }
+        }
+
+        for (pos = 1; pos < 20; pos++)
+        {
+            sum[pos] += sum[pos - 1];
         }
     }
 

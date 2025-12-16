5d715c0a942d61f5bf886f82c390c0e9a24253bf
LeoNg83 <85401996@qq.com>
2025-12-16 23:56:29 +0800

    init

---
+++ Toph/LIDS/main.cc
@@ -0,0 +1,43 @@
+// https://toph.co/p/lids
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <bitset>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // init
+    {
+
+    }
+
+    int t;
+    cin >> t;
+
+    for (int i = 1; i <= t; i++)
+    {
+        long long x, y;
+        cin >> x >> y;
+
+//        printf("Case %d: %lld %lld\n", i, x, y);
+    }
+
+    return 0;
+}

167f3544e45be5d485d8dd976c088a77c59f8587
LeoNg83 <85401996@qq.com>
2025-12-17 00:38:26 +0800

    init

--- Toph/LIDS/main.cc
+++ Toph/LIDS/main.cc
@@ -18,6 +18,8 @@
 
 using namespace std;
 
+long long dp[11][10][10];   // dp[pos][num][len]
+
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -25,7 +27,42 @@ int main()
 
     // init
     {
+        int pos = 0;
+        for (int n = 1; n < 10; n++)
+        {
+            dp[pos][n][1] = 1;
+        }
+       
+        for (; pos < 10; pos++)
+        {
+            for (int n = 1; n < 10; n++)
+            {
+                dp[pos + 1][n][1] = 1;
+            }
 
+            // pos => pos + 1
+            for (int num = 0; num < 10; num++)
+            {
+                for (int len = 0; len < 10; len++)
+                {
+                    dp[pos + 1][num][len] += dp[pos][num][len];
+                }
+            }
+
+            for (int num = 0; num < 10; num++)
+            {
+                for (int len = 0; len < 10; len++)
+                {
+                    if (dp[pos][num][len])
+                    {
+                        for (int sub = 1; sub < num; sub++)
+                        {
+                            dp[pos + 1][sub][len + 1] += dp[pos][num][len];
+                        }
+                    }
+                }
+            }
+        }
     }
 
     int t;

788eb91eb4191e6a503ce690b344eae26b15110a
LeoNg83 <85401996@qq.com>
2025-12-20 16:48:08 +0800

    init

--- HackerEarth/GreatNinjaWar/main.cc
+++ HackerEarth/GreatNinjaWar/main.cc
@@ -21,7 +21,7 @@ using namespace std;
 
 int lcm[48] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 15, 18, 20, 21, 24, 28, 30, 35, 36, 40, 42, 45, 56, 60, 63, 70, 72, 84, 90, 105, 120, 126, 140, 168, 180, 210, 252, 280, 315, 360, 420, 504, 630, 840, 1260, 2520};
 unordered_map<int, unordered_set<int>> mltmap;
-long long dp[13][1024][2520];   // dp[pos][mask][mod]
+long long dp[13][1024][2520]; // dp[pos][mask][mod]
 long long sum[13];
 long long powers[10];
 
@@ -80,7 +80,7 @@ void initDP(int mask, int mod, unordered_set<int> &st)
             dp[pos][1 << n][powers[n] % mod] = 1;
 
             auto search = st.find(1 << n);
-            if ( search != st.end())
+            if (search != st.end())
             {
                 sum[pos] += 1;
             }
@@ -117,13 +117,77 @@ void initDP(int mask, int mod, unordered_set<int> &st)
                 }
             }
         }
-        
+
+        sum[pos + 1] += sum[pos];
     }
 }
 
-long long func(int num, int mask, int mod)
+long long func(int num, int mask, int mod, unordered_set<int> &st)
 {
     long long ret = 0;
+
+    string strN = to_string(num);
+    int len = strN.length();
+
+    int preMask = 0;
+    int preMod = 0;
+    for (size_t pos = 0; pos < len; pos++)
+    {
+        int d = strN.at(pos) - '0';
+        int down = 0;
+        int up = d - 1;
+
+        if (0 == pos)
+        {
+            down = 1;
+
+            if (1 < len)
+            {
+                ret += sum[len - 2];
+            }
+        }
+
+        if (len - 1 == pos)
+        {
+            up = d;
+        }
+
+        for (size_t n = down; n <= up; n++)
+        {
+            if (mask & (1 << n))
+            {
+                int tmpMask = preMask | (1 << n);
+                int tmpMod = (preMod + powers[n]) % mod;
+
+                if (0 <= len - 2 - pos)
+                {
+                    for (size_t m = 0; m < 1024; m++)
+                    {
+                        auto search = st.find(tmpMask | m);
+                        if (search != st.end())
+                        {
+                            ret += dp[len - 2 - pos][m][(mod - tmpMod) % mod];
+                        }
+                    }
+                }
+                else
+                {
+                    if (0 == tmpMod)
+                    {
+                        auto search = st.find(tmpMask);
+                        if (search != st.end())
+                        {
+                            ret += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        preMask |= 1 << d;
+        preMod += powers[d];
+        preMod %= mod;
+    }
 
     return ret;
 }
@@ -151,7 +215,7 @@ int main()
     for (size_t i = 0; i < 48; i++)
     {
         int mod = lcm[i];
-        int mask = 1;   // 0 - valid
+        int mask = 1; // 0 - valid
 
         auto search = mltmap.find(mod);
         if (search != mltmap.end())
@@ -166,7 +230,7 @@ int main()
 
         for (size_t j = 0; j < q; j++)
         {
-            ans[j] += func(rs[j], mask, mod) - func(ls[j] - 1, mask, mod);
+            ans[j] += func(rs[j], mask, mod, search->second) - func(ls[j] - 1, mask, mod, search->second);
         }
     }
 

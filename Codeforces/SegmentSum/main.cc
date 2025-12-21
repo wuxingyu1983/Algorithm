636e7f80bdd23d1d4c09c4fac4d8dc0837df1a20
LeoNg83 <85401996@qq.com>
2025-12-22 00:32:22 +0800

    init

--- Codeforces/SegmentSum/main.cc
+++ Codeforces/SegmentSum/main.cc
@@ -19,13 +19,13 @@
 using namespace std;
 
 #pragma GCC target("popcnt")
-// __builtin_popcount(n)
 
 const long long mod = 998244353;
 
 long long powers[18];
 long long sums[18];
 long long dp[18][1024];
+long long cnts[18][1024];
 
 long long func(long long num, int k)
 {
@@ -51,7 +51,42 @@ int main()
             powers[i] = (powers[i - 1] * 10) % mod;
         }
         
+        int pos = 0;
+        for (int n = 0; n < 10; n++)
+        {
+            cnts[pos][1 << n] = 1;
 
+            dp[pos][1 << n] = n;
+            sums[pos] += n;
+        }
+        
+        for (; pos < 17; pos++)
+        {
+            for (int mask = 1; mask < 1024; mask++)
+            {
+                if (cnts[pos][mask])
+                {
+                    for (int n = 0; n < 10; n++)
+                    {
+                        int newMask = mask | (1 << n);
+                        if (k >= __builtin_popcount(newMask))
+                        {
+                            cnts[pos + 1][newMask] += cnts[pos][mask];
+
+                            long long tmp = (n * powers[pos + 1] * (cnts[pos][mask] % mod)) % mod;
+                            dp[pos + 1][newMask] += tmp;
+                            dp[pos + 1][newMask] %= mod;
+
+                            if (0 < n)
+                            {
+                                sums[pos + 1] += tmp;
+                                sums[pos + 1] %= mod;
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     cout << (mod + func(r, k) - func(l - 1, k)) % mod << endl;

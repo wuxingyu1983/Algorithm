dea1175104dc5d81f0cbe2dbbaf0b9a732435f9f
LeoNg83 <85401996@qq.com>
2025-12-23 00:38:32 +0800

    init

---
+++ Codeforces/SumsOfDigits/main.cc
@@ -0,0 +1,44 @@
+// https://codeforces.com/problemset/problem/509/C
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <bitset>
+
+using namespace std;
+
+string func(int b, string preA)
+{
+    string ret;
+
+    return ret;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    string a;
+    for (size_t i = 0; i < n; i++)
+    {
+        int b;
+        cin >> b;
+
+        a = func(b, a);
+        cout << a << endl;
+    }
+
+    return 0;
+}

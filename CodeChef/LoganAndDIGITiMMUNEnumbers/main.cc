43f48a9005f41ee52baa1aa6b12e1865317414d7
LeoNg83 <85401996@qq.com>
2025-12-29 01:41:40 +0800

    init

--- CodeChef/LoganAndDIGITiMMUNEnumbers/main.cc
+++ CodeChef/LoganAndDIGITiMMUNEnumbers/main.cc
@@ -27,17 +27,47 @@ int mask[4] = {3, 28, 224, 3840};
 
 void init()
 {
-    power[0][0] = 1;
-    power[0][1] = 1;
-    power[0][2] = 1;
-    power[0][3] = 1;
-    for (int pos = 1; pos < 19; pos++)
+    int pos = 0;
+    power[pos][0] = 1;
+    power[pos][1] = 1;
+    power[pos][2] = 1;
+    power[pos][3] = 1;
+    for (pos = 1; pos < 19; pos++)
     {
         for (int i = 0; i < 4; i++)
         {
             power[pos][i] = (power[pos - 1][i] * 10) % odds[i];
         }
     }
+
+    pos = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        int num = odds[i];
+        int mod = 0;
+        for (int j = 0; j < 4; j++)
+        {
+            mod |= (num % odds[j]) << offset[j];
+        }
+        
+        dp[pos][1 << i][mod] = 1;
+    }
+    
+    for (; pos < 18; pos++)
+    {
+        for (int oldSt = 1; oldSt < 16; oldSt++)
+        {
+            for (int oldMod = 0; oldMod < 4096; oldMod++)
+            {
+                if (dp[pos][oldSt][oldMod])
+                {
+                    
+                }
+            }
+        }
+        
+    }
+    
 }
 
 int main()

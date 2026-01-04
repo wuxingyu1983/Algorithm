7a21a6193819d363818f8547044429428ff4c656
LeoNg83 <85401996@qq.com>
2026-01-04 20:29:54 +0800

    init

---
+++ CodeChef/ChefAndSpecialNumbers/main.cc
@@ -0,0 +1,26 @@
+// https://www.codechef.com/problems/WORKCHEF?tab=statement
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <bitset>
+
+using namespace std;
+
+#pragma GCC target("popcnt")
+
+int main()
+{
+    return 0;
+}

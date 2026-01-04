c8cb348b3a60bd1475c913f78b66f1677626b513
LeoNg83 <85401996@qq.com>
2026-01-04 20:55:00 +0800

    init

--- CodeChef/ChefAndSpecialNumbers/main.cc
+++ CodeChef/ChefAndSpecialNumbers/main.cc
@@ -20,7 +20,42 @@ using namespace std;
 
 #pragma GCC target("popcnt")
 
+long long powers[19];
+unordered_multimap<int, int> mltmap;
+
+int gcd(int a, int b)
+{
+    if (a % b == 0)
+        return b;
+    else
+        return gcd(b, a % b);
+}
+
+void init()
+{
+    for (int n = 1; n < 1024; n++)
+    {
+        int l = 1;
+        for (int idx = 0; idx < 10; idx++)
+        {
+            if (n & (1 << idx))
+            {
+                l = l * idx / gcd(l, idx);
+            }
+        }
+
+        mltmap.insert(make_pair<int, int>(__builtin_popcount(n), (int)l));
+    }
+}
+
 int main()
 {
+    int q;
+    cin >> q;
+
+    init();
+
+    
+
     return 0;
 }

2842b3e80fed1d486a06a245b8dc6bae992e80de
LeoNg83 <85401996@qq.com>
2026-01-04 21:42:08 +0800

    init

--- CodeChef/ChefAndSpecialNumbers/main.cc
+++ CodeChef/ChefAndSpecialNumbers/main.cc
@@ -31,21 +31,16 @@ int gcd(int a, int b)
         return gcd(b, a % b);
 }
 
-void init()
+void init(int k, int mod)
 {
-    for (int n = 1; n < 1024; n++)
-    {
-        int l = 1;
-        for (int idx = 0; idx < 10; idx++)
-        {
-            if (n & (1 << idx))
-            {
-                l = l * idx / gcd(l, idx);
-            }
-        }
+    
+}
 
-        mltmap.insert(make_pair<int, int>(__builtin_popcount(n), (int)l));
-    }
+long long getCnt(long long num, int k, int mod)
+{
+    long long ret = 0;
+
+    return ret;
 }
 
 int main()
@@ -53,9 +48,54 @@ int main()
     int q;
     cin >> q;
 
-    init();
+    // init
+    {
+        for (int n = 1; n < 1024; n++)
+        {
+            int l = 1;
+            for (int idx = 0; idx < 10; idx++)
+            {
+                if (n & (1 << idx))
+                {
+                    l = l * idx / gcd(l, idx);
+                }
+            }
 
-    
+            mltmap.insert(make_pair<int, int>(__builtin_popcount(n), (int)l));
+        }
+    }
+
+    for (int i = 0; i < q; i++)
+    {
+        long long l, r;
+        int k;
+
+        cin >> l >> r >> k;
+
+        long long cntL = 0, cntR = 0;
+        auto range = mltmap.equal_range(k);
+        for (auto it = range.first; it != range.second; ++it)
+        {
+            init(k, it->second);
+
+            cntL += getCnt(l - 1, k, it->second);
+            cntR += getCnt(r, k, it->second);
+        }
+
+        if ((k & 1) && (k != 9))
+        {
+            range = mltmap.equal_range(9);
+            for (auto it = range.first; it != range.second; ++it)
+            {
+                init(9, it->second);
+
+                cntL += getCnt(l - 1, 9, it->second);
+                cntR += getCnt(r, 9, it->second);
+            }
+        }
+
+        cout << cntR - cntL << endl;
+    }
 
     return 0;
 }

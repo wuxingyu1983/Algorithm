81ae036ef3e18a254e8beabd1bc2d555b6d05081
LeoNg83 <85401996@qq.com>
2026-01-16 01:04:10 +0800

    init

---
+++ Luogu/P3763/main.cc
@@ -0,0 +1,46 @@
+// https://www.luogu.com.cn/problem/P3763
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <bitset>
+
+using namespace std;
+
+constexpr int M = 1e9 + 7;
+constexpr int B = 233;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        string s, p;
+        cin >> s;
+        cin >> p;
+
+        if (s.length() < p.length())
+        {
+            cout << 0 << endl;
+        }
+        else
+        {
+            
+        }
+    }
+
+    return 0;
+}

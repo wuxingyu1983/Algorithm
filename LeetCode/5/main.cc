6ca5fd15844b8069e7dab173fedf2173729ec1b6
LeoNg83 <85401996@qq.com>
2026-01-18 01:05:21 +0800

    init

---
+++ LeetCode/5/main.cc
@@ -0,0 +1,36 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+class Solution
+{
+public:
+    string longestPalindrome(string s)
+    {
+        string ret;
+
+        return ret;
+    }
+};
+
+int main()
+{
+    string str;
+    cin >> str;
+
+    Solution s;
+    cout << s.longestPalindrome(str) << endl;
+
+    return 0;
+}

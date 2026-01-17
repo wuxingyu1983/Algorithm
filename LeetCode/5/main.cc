e4a2446dbae069e08d1bcaa606228f8bebc79ee3
LeoNg83 <85401996@qq.com>
2026-01-18 02:52:44 +0800

    AC

--- LeetCode/5/main.cc
+++ LeetCode/5/main.cc
@@ -18,10 +18,48 @@ class Solution
 public:
     string longestPalindrome(string s)
     {
-        string ret;
+        memset(dp, 0, sizeof(dp));
 
+        int start = 0;
+        int max = 1;
+        int len = s.length();
+
+        for (int pos = 0; pos < len; pos++)
+        {
+            dp[pos][1] = 1;
+        }
+
+        for (int step = 2; step <= len; step++)
+        {
+            for (int pos = 0; pos <= len - step; pos++)
+            {
+                if (s.at(pos) == s.at(pos + step - 1))
+                {
+                    if (2 == step)
+                    {
+                        dp[pos][step] = 1;
+                        start = pos;
+                        max = step;
+                    }
+                    else
+                    {
+                        dp[pos][step] = dp[pos + 1][step - 2];
+                        if (1 == dp[pos][step])
+                        {
+                            start = pos;
+                            max = step;
+                        }
+                    }
+                }
+            }
+        }
+
+        string ret(s.begin() + start, s.begin() + start + max);
         return ret;
     }
+
+private:
+    char dp[1001][1001]; // dp[pos][len]
 };
 
 int main()

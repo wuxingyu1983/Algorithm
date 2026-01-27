a3bb85979c81398f40120bbdd0be6b7121ee050a
LeoNg83 <85401996@qq.com>
2026-01-28 02:09:12 +0800

    init

---
+++ OnlineJudge/ExtendToPalindrome/main.cc
@@ -0,0 +1,33 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+string extend(string &s)
+{
+    string ret(s);
+
+    return ret;
+}
+
+int main()
+{
+    string str;
+
+    while (cin >> str)
+    {
+        cout << extend(str) << endl;
+    }
+
+    return 0;
+}

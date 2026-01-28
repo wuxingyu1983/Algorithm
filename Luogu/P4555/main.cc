b064e75bf0d6f840a2dd7bcb53c109b5eeb628c7
LeoNg83 <85401996@qq.com>
2026-01-29 00:49:20 +0800

    init

--- Luogu/P4555/main.cc
+++ Luogu/P4555/main.cc
@@ -16,6 +16,7 @@
 using namespace std;
 
 int P[200005];
+int L[200005];
 
 string preprocString(string &s)
 {
@@ -37,7 +38,7 @@ int main()
     cin >> str;
 
     string sp = preprocString(str);
-    
+
     int n = sp.length();
     int C = 0, R = 0;
     for (int i = 1; i < n - 1; i++)
@@ -50,11 +51,16 @@ int main()
         else
         {
             P[i] = 0; // 等于 R 的情况
+            if (0 == L[i])
+            {
+                L[i] = 1;
+            }
         }
 
         // 碰到之前讲的三种情况时候，需要利用中心扩展法
         while (sp.at(i + 1 + P[i]) == sp.at(i - 1 - P[i]))
         {
+            L[i + 1 + P[i]] = 2 * P[i] + 3;
             P[i]++;
         }
 

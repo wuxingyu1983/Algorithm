90cdb1ad43dfeba34e2bb0ef4420a5cb740a0c98
LeoNg83 <85401996@qq.com>
2026-01-31 03:05:14 +0800

    AC

--- Codeforces/PrefixSuffixPalindrome/main.cc
+++ Codeforces/PrefixSuffixPalindrome/main.cc
@@ -15,7 +15,7 @@
 
 using namespace std;
 
-int P[2000005];
+int P[2000100];
 
 string preprocString(string &s)
 {
@@ -33,11 +33,13 @@ string preprocString(string &s)
 
 string func(string &str)
 {
+    memset(P, 0, sizeof(int) * (str.length() * 2 + 3));
+
     string ret;
     int l = 0, r = str.length() - 1;
     string pre;
 
-    if (l <= r && str.at(l) == str.at(r))
+    while (l <= r && str.at(l) == str.at(r))
     {
         pre += str.at(l);
         l ++;
@@ -80,6 +82,24 @@ string func(string &str)
                 R = i + P[i];
             }
         }
+
+        int max = 0;
+        int centerIndex = 0;
+        for (int i = 1; i < n - 1; i++)
+        {
+            if (P[i] > max && (1 == i - P[i] || n - 2 == i + P[i]))
+            {
+                max = P[i];
+                centerIndex = i;
+            }
+        }
+        int start = (centerIndex - max) / 2; // 最开始讲的求原字符串下标
+        string mid(s.begin() + start, s.begin() + start + max);
+
+        ret += pre;
+        ret += mid;
+        reverse(pre.begin(), pre.end());
+        ret += pre;
     }
 
     return ret;
@@ -87,6 +107,9 @@ string func(string &str)
 
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int t;
     cin >> t;
 
@@ -95,7 +118,7 @@ int main()
         string str;
         cin >> str;
 
-        cout << func(str) << endl;
+        cout << func(str) << "\n";
     }
     
     return 0;

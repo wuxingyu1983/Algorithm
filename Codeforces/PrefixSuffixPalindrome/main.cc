49e1ab8c8f629910909de05b54e93ea23152d592
LeoNg83 <85401996@qq.com>
2026-01-31 02:45:42 +0800

    init

--- Codeforces/PrefixSuffixPalindrome/main.cc
+++ Codeforces/PrefixSuffixPalindrome/main.cc
@@ -15,6 +15,8 @@
 
 using namespace std;
 
+int P[2000005];
+
 string preprocString(string &s)
 {
     string ret = "^";
@@ -50,8 +52,34 @@ string func(string &str)
     {
         string s(str, l, r - l + 1);
         string sp = preprocString(s);
+        int n = sp.length();
+        int C = 0, R = 0;
 
-        
+        for (int i = 1; i < n - 1; i++)
+        {
+            int i_mirror = 2 * C - i;
+            if (R > i)
+            {
+                P[i] = min(R - i, P[i_mirror]); // 防止超出 R
+            }
+            else
+            {
+                P[i] = 0; // 等于 R 的情况
+            }
+
+            // 碰到之前讲的三种情况时候，需要利用中心扩展法
+            while (sp.at(i + 1 + P[i]) == sp.at(i - 1 - P[i]))
+            {
+                P[i]++;
+            }
+
+            // 判断是否需要更新 R
+            if (i + P[i] > R)
+            {
+                C = i;
+                R = i + P[i];
+            }
+        }
     }
 
     return ret;

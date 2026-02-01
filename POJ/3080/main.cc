af366b831ddddd13cc57727d0d98506a40c47c28
LeoNg83 <85401996@qq.com>
2026-02-01 13:35:10 +0800

    init

---
+++ POJ/3080/main.cc
@@ -0,0 +1,28 @@
+// http://poj.org/problem?id=3080
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    for (int it = 0; it < t; it++)
+    {
+    }
+
+    return 0;
+}

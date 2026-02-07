52e3a6714833a63848f56f09bdf0d939f01fd770
LeoNg83 <85401996@qq.com>
2026-02-07 23:40:20 +0800

    init

---
+++ Codeforces/CompressWords/main.cc
@@ -0,0 +1,34 @@
+// https://codeforces.com/contest/1200/problem/E
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    string item;
+    vector<string> words;
+
+    // 默认按空格分割
+    while (getline(cin, item, ' '))
+    {
+        words.push_back(item);
+    }
+
+    return 0;
+}

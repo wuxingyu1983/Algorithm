387e1fb338f08db9eb5215e370ff7b0f3f15fc50
LeoNg83 <85401996@qq.com>
2026-03-17 02:16:24 +0800

    init

--- Codeforces/VasiliysMultiset/main.cc
+++ Codeforces/VasiliysMultiset/main.cc
@@ -16,6 +16,54 @@
 
 using namespace std;
 
+const int MAX_N = 200000;
+const int MAXH = 32;
+const int MAX_NODES = MAXH * MAX_N;
+int ch[MAX_NODES][2], w[MAX_NODES], xorv[MAX_NODES], root;
+int tot = 0;
+
+
+int mknode()
+{
+    ++tot;
+    ch[tot][1] = ch[tot][0] = w[tot] = xorv[tot] = 0;
+    return tot;
+}
+
+void maintain(int o)
+{
+    w[o] = xorv[o] = 0;
+    if (ch[o][0])
+    {
+        w[o] += w[ch[o][0]];
+        xorv[o] ^= xorv[ch[o][0]] << 1;
+    }
+    if (ch[o][1])
+    {
+        w[o] += w[ch[o][1]];
+        xorv[o] ^= (xorv[ch[o][1]] << 1) | (w[ch[o][1]] & 1);
+    }
+    w[o] = w[o] & 1;
+}
+
+void insert(int &o, int x, int dp)
+{
+    if (!o)
+        o = mknode();
+    if (dp >= MAXH - 1)
+        return (void)(w[o]++);
+    insert(ch[o][(x >> (MAXH - 1 - dp)) & 1], x, dp + 1);
+    maintain(o);
+}
+
+void erase(int o, int x, int dp)
+{
+    if (dp >= MAXH - 1)
+        return (void)(w[o]--);
+    erase(ch[o][(x >> (MAXH - 1 - dp)) & 1], x, dp + 1);
+    maintain(o);
+}
+
 int main()
 {
     int q;
@@ -32,15 +80,19 @@ int main()
         {
         case '+':
         {
+            insert(root, x, 0);
         }
         break;
         case '-':
         {
+            erase(root, x, 0);
         }
         break;
         case '?':
         default:
-            break;
+        {
+        }
+        break;
         }
     }
 

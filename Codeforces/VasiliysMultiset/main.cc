1314a5db42413f8e57000b4eb5e8d46b883f3e3a
LeoNg83 <85401996@qq.com>
2026-03-17 00:47:47 +0800

    init

---
+++ Codeforces/VasiliysMultiset/main.cc
@@ -0,0 +1,48 @@
+// https://codeforces.com/problemset/problem/706/D
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    int q;
+    scanf("%d", &q);
+
+    for (size_t i = 0; i < q; i++)
+    {
+        char op;
+        int x;
+
+        scanf("%c %d", &op, &x);
+
+        switch (op)
+        {
+        case '+':
+        {
+        }
+        break;
+        case '-':
+        {
+        }
+        break;
+        case '?':
+        default:
+            break;
+        }
+    }
+
+    return 0;
+}

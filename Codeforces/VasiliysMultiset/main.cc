26afb26f7e7394addb8c4275ede3a1a78d15ae29
LeoNg83 <85401996@qq.com>
2026-03-17 22:04:41 +0800

    AC

--- Codeforces/VasiliysMultiset/main.cc
+++ Codeforces/VasiliysMultiset/main.cc
@@ -19,17 +19,18 @@ using namespace std;
 const int MAX_N = 200000;
 const int MAXH = 32;
 const int MAX_NODES = MAXH * MAX_N;
-int ch[MAX_NODES][2], w[MAX_NODES], xorv[MAX_NODES], root;
+int ch[MAX_NODES][2], /*w[MAX_NODES], xorv[MAX_NODES],*/ root, cnt[MAX_NODES];
 int tot = 0;
 
 
 int mknode()
 {
     ++tot;
-    ch[tot][1] = ch[tot][0] = w[tot] = xorv[tot] = 0;
+    ch[tot][1] = ch[tot][0] /* = w[tot] = xorv[tot]*/ = 0;
     return tot;
 }
 
+/*
 void maintain(int o)
 {
     w[o] = xorv[o] = 0;
@@ -45,36 +46,43 @@ void maintain(int o)
     }
     w[o] = w[o] & 1;
 }
+*/
 
 void insert(int &o, int x, int dp)
 {
     if (!o)
         o = mknode();
-    if (dp >= MAXH - 1)
-        return (void)(w[o]++);
+    cnt[o] ++;
+    if (dp >= MAXH)
+//        return (void)(w[o]++);
+        return;
     insert(ch[o][(x >> (MAXH - 1 - dp)) & 1], x, dp + 1);
-    maintain(o);
+//    maintain(o);
 }
 
 void erase(int o, int x, int dp)
 {
-    if (dp >= MAXH - 1)
-        return (void)(w[o]--);
+    cnt[o] --;
+    if (dp >= MAXH)
+//        return (void)(w[o]--);
+        return;
     erase(ch[o][(x >> (MAXH - 1 - dp)) & 1], x, dp + 1);
-    maintain(o);
+//    maintain(o);
 }
 
 int main()
 {
     int q;
-    scanf("%d", &q);
+    scanf("%d\n", &q);
+
+    insert(root, 0, 0);
 
     for (size_t i = 0; i < q; i++)
     {
         char op;
         int x;
 
-        scanf("%c %d", &op, &x);
+        scanf("%c %d\n", &op, &x);
 
         switch (op)
         {
@@ -91,6 +99,23 @@ int main()
         case '?':
         default:
         {
+            int ret = 0;
+            int p = root;
+            for (int i = 31; i >= 0; i--)
+            {
+                int c = (x >> i) & 1;
+                if (0 < ch[p][1 - c] && 0 < cnt[ch[p][1 - c]])
+                {
+                    ret |= 1 << i;
+                    p = ch[p][1 - c];
+                }
+                else
+                {
+                    p = ch[p][c];
+                }
+            }
+            
+            printf("%d\n", ret);
         }
         break;
         }

c4e808385276e67cc256b67bfbf7d12595c7da7d
LeoNg83 <85401996@qq.com>
2026-04-26 19:04:31 +0800

    init

--- LeetCode/673/main.cc
+++ LeetCode/673/main.cc
@@ -19,7 +19,7 @@ const int MAX_N = 2001;
 unordered_map<int, int> indexs;
 
 // root 表示整棵线段树的根结点；cnt 表示当前结点个数
-int cnt, root[MAX_N];
+int cnt, root[MAX_N], lis[MAX_N];
 int sum[MAX_N * 2 * 11], ls[MAX_N * 2 * 11], rs[MAX_N * 2 * 11];
 
 class Solution
@@ -40,12 +40,33 @@ public:
             indexs.insert({tmp[i], 1 + i});
         }
 
-        return 0;
+        vector<int> d(MAX_N * 11, 0);
+        int n = tmp.size();
+        int maxLen = 0;
+
+        for (size_t i = 0; i < nums.size(); i++)
+        {
+            int idx = indexs[nums[i]];
+
+            // 获取 1 - (idx - 1) 的最长len
+            int len = getMax(d, 1, idx - 1, 1, n, 1);
+            updateMax(d, idx, len + 1, 1, n, 1);
+
+            maxLen = max(maxLen, len + 1);
+
+            // 更新 sum
+            int preSum = getSum(root[len], 1, n, 1, idx - 1);
+            updateSum(root[len + 1], 1, n, idx, preSum);
+        }
+
+        ret = getSum(root[maxLen], 1, n, 1, n);
+
+        return ret;
     }
 
 private:
     // 用法：update(root, 1, n, x, f); 其中 x 为待修改节点的编号
-    void updateMax(int &p, int s, int t, int x, int f)
+    void updateSum(int &p, int s, int t, int x, int f)
     { // 引用传参
         if (!p)
             p = ++cnt; // 当结点为空时，创建一个新的结点
@@ -56,9 +77,9 @@ private:
         }
         int m = s + ((t - s) >> 1);
         if (x <= m)
-            updateMax(ls[p], s, m, x, f);
+            updateSum(ls[p], s, m, x, f);
         else
-            updateMax(rs[p], m + 1, t, x, f);
+            updateSum(rs[p], m + 1, t, x, f);
         sum[p] = sum[ls[p]] + sum[rs[p]]; // pushup
     }
 
@@ -75,6 +96,44 @@ private:
         if (r > m)
             ans += getSum(rs[p], m + 1, t, l, r);
         return ans;
+    }
+
+    // 合并左右子树的最大值
+    void push_up(vector<int> &tree, int p)
+    {
+        tree[p] = max(tree[p << 1], tree[p << 1 | 1]);
+    }
+
+    void updateMax(vector<int> &tree, int idx, int c, int l, int r, int p)
+    {
+        if (idx == l && idx == r)
+        {
+            tree[p] = c;
+            return;
+        }
+
+        int mid = (l + r) >> 1;
+        if (idx <= mid)
+            updateMax(tree, idx, c, l, mid, p << 1);
+        else
+            updateMax(tree, idx, c, mid + 1, r, p << 1 | 1);
+        push_up(tree, p);
+    }
+
+    // 区间查询：查询[L, R]内的最大值
+    int getMax(vector<int> &tree, int L, int R, int l, int r, int p)
+    {
+        if (L > R)
+            return 0;
+        if (L <= l && r <= R)
+            return tree[p]; // 完全覆盖
+        int mid = (l + r) >> 1;
+        int res = 0;
+        if (L <= mid)
+            res = max(res, getMax(tree, L, R, l, mid, p << 1));
+        if (R > mid)
+            res = max(res, getMax(tree, L, R, mid + 1, r, p << 1 | 1));
+        return res;
     }
 };
 

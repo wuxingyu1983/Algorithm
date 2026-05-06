546319481c7c7ccadf7af672720a1782af1c31f5
LeoNg83 <85401996@qq.com>
2026-05-06 15:31:13 +0800

    init

--- Codeforces/Tourist/main.cc
+++ Codeforces/Tourist/main.cc
@@ -43,6 +43,44 @@ bool myfunc(Event &a, Event &b)
     return bRet;
 }
 
+// 合并左右子树的最大值
+void push_up(vector<int> &tree, int p)
+{
+    tree[p] = max(tree[p << 1], tree[p << 1 | 1]);
+}
+
+void updateMax(vector<int> &tree, int idx, int c, int l, int r, int p)
+{
+    if (idx == l && idx == r)
+    {
+        tree[p] = c;
+        return;
+    }
+
+    int mid = (l + r) >> 1;
+    if (idx <= mid)
+        updateMax(tree, idx, c, l, mid, p << 1);
+    else
+        updateMax(tree, idx, c, mid + 1, r, p << 1 | 1);
+    push_up(tree, p);
+}
+
+// 区间查询：查询[L, R]内的最大值
+int getMax(vector<int> &tree, int L, int R, int l, int r, int p)
+{
+    if (L > R)
+        return 0;
+    if (L <= l && r <= R)
+        return tree[p]; // 完全覆盖
+    int mid = (l + r) >> 1;
+    int res = 0;
+    if (L <= mid)
+        res = max(res, getMax(tree, L, R, l, mid, p << 1));
+    if (R > mid)
+        res = max(res, getMax(tree, L, R, mid + 1, r, p << 1 | 1));
+    return res;
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -83,7 +121,7 @@ int main()
         xToIdx.insert({xs[i], i + 1});
     }
 
-    
+
 
     return 0;
 }

df306081cabde2d6fbb37b56f9ed46850e62b643
LeoNg83 <85401996@qq.com>
2026-05-06 12:15:05 +0800

    init

--- Codeforces/Tourist/main.cc
+++ Codeforces/Tourist/main.cc
@@ -16,8 +16,38 @@
 
 using namespace std;
 
+const int MAX_N = 100000;
+
+class Event 
+{
+public:
+    int x, t;
+};
+
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    vector<Event> events;
+    for (size_t i = 0; i < n; i++)
+    {
+        int x, t;
+        cin >> x >> t;
+
+        Event e;
+        e.x = x;
+        e.t = t;
+
+        events.push_back(e);
+    }
+    
+    int v;
+    cin >> v;
+    
 
     return 0;
 }

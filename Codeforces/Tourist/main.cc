b796dfce9959727e901169f7574c42c943a0d5af
LeoNg83 <85401996@qq.com>
2026-05-06 12:09:16 +0800

    init

---
+++ Codeforces/Tourist/main.cc
@@ -0,0 +1,23 @@
+// https://codeforces.com/contest/76/problem/F
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}

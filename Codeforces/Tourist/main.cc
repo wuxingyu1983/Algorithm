86091ade0ac260d769453d8451b70125ec040679
LeoNg83 <85401996@qq.com>
2026-05-16 16:16:27 +0800

    init

--- Codeforces/Tourist/main.cc
+++ Codeforces/Tourist/main.cc
@@ -18,6 +18,160 @@ using namespace std;
 
 const int MAX_N = 100000;
 
+class node_y
+{
+public:
+    int l, r;
+    int res;
+
+    node_y * lson;
+    node_y * rson;
+
+    node_y(int _l, int _r) : l(_l), r(_r)
+    {
+        lson = rson = NULL;
+        res = -1;
+    }
+};
+
+// 内层线段树
+class tree_y
+{
+public:
+    node_y root;
+
+    tree_y(int _m) : root(1, _m)
+    {
+    }
+
+    void update(node_y * now, int y, int k)
+    {
+        if (now->l == now->r)
+        {
+            now->res = max(now->res, k);
+            return;
+        }
+        int mid = (now->l + now->r) >> 1;
+        if (y <= mid)
+        {
+            if (NULL == now->lson)
+            {
+                now->lson = new node_y(now->l, mid);
+            }
+            update(now->lson, y, k);
+        }
+        else
+        {
+            if (NULL == now->rson)
+            {
+                now->rson = new node_y(mid + 1, now->r);
+            }
+            update(now->rson, y, k);
+        }
+
+        push_up(now);
+    }
+
+    int query(node_y * now, int l, int r)
+    {
+        if (NULL == now)
+            return -1;
+        if (now->l >= l && now->r <= r)
+            return now->res;
+        int mid = (now->l + now->r) >> 1;
+        if (r <= mid)
+            return query(now->lson, l, r);
+        else if (l > mid)
+            return query(now->rson, l, r);
+        else
+            return max(query(now->lson, l, mid), query(now->rson, mid + 1, r));
+    }
+
+private:
+    void push_up(node_y * now)
+    {
+        if (now)
+        {
+            if (now->lson)
+            {
+                now->res = now->lson->res;
+            }
+
+            if (now->rson && now->res < now->rson->res)
+            {
+                now->res = now->rson->res;
+            }
+        }
+    }
+};
+
+class node_x
+{
+public:
+    int l, r;
+    tree_y tr;
+    node_x * lson;
+    node_x * rson;
+
+    node_x(int _l, int _r, int _y) : l(_l), r(_r), tr(_y)
+    {
+        lson = rson = NULL;
+    }
+};
+
+class tree_x
+{
+public:
+    node_x root;
+    int maxy;
+
+    tree_x(int _maxx, int _maxy) : root(1, _maxx, _maxy)
+    {
+        maxy = _maxy;
+    }
+
+    void update(node_x * now, int x, int y, int k)
+    {
+        now->tr.update(&(now->tr.root), y, k);
+        if (now->l == now->r)
+            return;
+        int mid = (now->l + now->r) >> 1;
+        if (x <= mid)
+        {
+            if (NULL == now->lson)
+            {
+                now->lson = new node_x(now->l, mid, maxy);
+            }
+            update(now->lson, x, y, k);
+        }
+        else
+        {
+            if (NULL == now->rson)
+            {
+                now->rson = new node_x(mid + 1, now->r, maxy);
+            }
+            update(now->rson, x, y, k);
+        }
+    }
+
+    int query(node_x * now, int lx, int rx, int ly, int ry)
+    {
+        if (NULL == now)
+            return -1;
+        if (now->l >= lx && now->r <= rx)
+        {
+            return now->tr.query(&(now->tr.root), ly, ry);
+        }
+        int mid = (now->l + now->r) >> 1;
+        if (rx <= mid)
+            return query(now->lson, lx, rx, ly, ry);
+        else if (lx > mid)
+            return query(now->rson, lx, rx, ly, ry);
+        else
+            return max(query(now->lson, lx, mid, ly, ry), query(now->rson, mid + 1, rx, ly, ry));
+    }
+};
+
 class Event 
 {
 public:
@@ -89,14 +243,22 @@ int main()
         it = lower_bound(bs.begin(), bs.end(), events[i].b);
         events[i].bi = it - bs.begin() + 1; 
     }
-
+    
     int rslt = 0;
     
+    // 情况 1
+    {
+
+    }
     
     cout << rslt << " ";
 
     rslt = 0;
 
+    // 情况 2
+    {
+
+    }
 
     cout << rslt << "\n";
 

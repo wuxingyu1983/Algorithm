5fed577022e69490935040937b4a4efbca37f66e
LeoNg83 <85401996@qq.com>
2026-06-06 15:22:57 +0800

    init

---
+++ OnlineJudge/HappyBirthday/main.cc
@@ -0,0 +1,23 @@
+// https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&page=show_problem&problem=3153
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <utility>
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}

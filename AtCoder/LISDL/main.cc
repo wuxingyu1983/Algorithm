29c092318b40823e8c517438a1de0fa7682d53d7
LeoNg83 <85401996@qq.com>
2026-06-20 13:53:27 +0800

    init

---
+++ AtCoder/LISDL/main.cc
@@ -0,0 +1,38 @@
+// https://atcoder.jp/contests/arc091/tasks/arc091_c
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <utility>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, a, b;
+    cin >> n >> a >> b;
+
+    if (n < a + b - 1)
+    {
+        cout << "-1\n";
+    }
+    else
+    {
+        
+    }
+
+    return 0;
+}

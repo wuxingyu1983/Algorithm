a838a3925889f1e177709b37483e5f5bd05a2c23
LeoNg83 <85401996@qq.com>
2026-07-04 01:04:03 +0800

    init

---
+++ dmoj/Selotejp/main.cc
@@ -0,0 +1,28 @@
+// https://dmoj.ca/problem/coci20c3p4
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <utility>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    
+
+    return 0;
+}

7f283fb73500cdec891987bc2a6c1496a78c69e4
LeoNg83 <85401996@qq.com>
2026-07-15 22:22:33 +0800

    init

--- TLX/StoringEggs/main.cc
+++ TLX/StoringEggs/main.cc
@@ -18,10 +18,10 @@
 using namespace std;
 
 const int MAX_N = 100;
-char cells[MAX_N][3];
+char cells[MAX_N];  // 每行为 3 bits 的状态
 
 short dp0[8][8][99];    // dp[st1][st2][interval] : 第i行的状态st1,第j行的状态st2,i行和j行中间还有interval行的最大距离
-
+char flags[8][8];
 
 int main()
 {
@@ -36,11 +36,17 @@ int main()
     {
         for (int j = 0; j < n; j++)
         {
-            cin >> cells[j][i];
+            char ch;
+            cin >> ch;
 
-            if ('.' == cells[j][i])
+            if ('.' == cells[j])
             {
                 usable ++;
+            }
+            else
+            {
+                // unusable cell, mark it in the bitmask
+                cells[j] |= 1 << i;
             }
         }
 
@@ -65,6 +71,9 @@ int main()
             
             for (int st2 = 0; st2 < 8; st2++)
             {
+                if (1 == flags[st1][st2])
+                    continue;
+
                 if (7 == st2 || 3 == st2 || 6 == st2)
                 {
                     if (0 == max || max > 1)
@@ -75,7 +84,26 @@ int main()
                     if (0 == max)
                         max = 4;
                 }
+
+                for (int inter = 0; inter < 99; inter++)
+                {
+                    if (1 == max)
+                    {
+                        dp0[st1][st2][inter] = 1;
+                        dp0[st2][st1][inter] = 1;
+                    }
+                    else
+                    {
+                        // 4 == max or 0 == max
+                        
+
+                    }
+                }
+
+                flags[st1][st2] = 1;
+                flags[st2][st1] = 1;
             }
+
         }
     }
     

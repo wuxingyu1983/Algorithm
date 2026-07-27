a164a53919559ca2d5a3701b2372d5044193dffd
LeoNg83 <85401996@qq.com>
2026-07-28 01:39:02 +0800

    init

---
+++ Codeforces/WiFi/main.cc
@@ -0,0 +1,39 @@
+// https://codeforces.com/contest/1216/problem/F
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+
+using namespace std;
+
+const int MAXN = 200010;
+
+char rooms[MAXN];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, k;
+    cin >> n >> k;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> rooms[i];
+    }
+
+    
+
+    return 0;
+}

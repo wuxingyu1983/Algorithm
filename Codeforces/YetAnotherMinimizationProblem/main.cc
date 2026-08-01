a258a60629794c529f7b21821e1199fe7bcbd29b
LeoNg83 <85401996@qq.com>
2026-08-01 13:34:46 +0800

    init

---
+++ Codeforces/YetAnotherMinimizationProblem/main.cc
@@ -0,0 +1,35 @@
+// https://codeforces.com/contest/1637/problem/D
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    for (int iT = 0; iT < t; iT++)
+    {
+        long long ans = 0;
+
+        cout << ans << "\n";
+    }
+
+    return 0;
+}

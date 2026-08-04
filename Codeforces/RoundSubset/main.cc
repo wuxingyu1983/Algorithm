562297e4b45972fb6987ab4c1cd2a40105bbc240
LeoNg83 <85401996@qq.com>
2026-08-04 21:48:58 +0800

    init

--- Codeforces/RoundSubset/main.cc
+++ Codeforces/RoundSubset/main.cc
@@ -13,14 +13,16 @@
 #include <queue>
 #include <stack>
 #include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
 const int MAXN = 201;
 const int mask = 16383;
 const int DIGITS = 14;
-int cnt2[MAXN], cnt5[MAXN];
+vector<int> cnt2, cnt5;
 unordered_set<int> dp[MAXN];      // dp[k]
+unordered_map<int, int> idxs;
 
 int main()
 {
@@ -36,25 +38,34 @@ int main()
         cin >> a;
 
         long long tmp = a;
+        int c2 = 0;
         while (tmp % 2 == 0)
         {
-            cnt2[i]++;
+            c2++;
             tmp /= 2;
         }
         
         tmp = a;
+        int c5 = 0;
         while (tmp % 5 == 0)
         {
-            cnt5[i]++;
+            c5++;
             tmp /= 5;
+        }
+
+        if (0 < c2 || 0 < c5)
+        {
+            cnt2.push_back(c2);
+            cnt5.push_back(c5);
         }
     }
 
     int ans = 0;
-
-    for (int i = 0; i < n; i++)
+    int n1 = cnt2.size();
+    int n0 = n - n1;
+    for (int i = 0; i < n1; i++)
     {
-        for (int j = k - 1; j >= max(k + i - n, 0); j--)
+        for (int j = min(k, i) - 1; j >= max(k - n0 + i - n1, 0); j--)
         {
             for (auto it = dp[j].begin(); it != dp[j].end(); it++)
             {
@@ -62,20 +73,40 @@ int main()
                 int old5 = *it & mask;
                 int new2 = old2 + cnt2[i];
                 int new5 = old5 + cnt5[i];
-                dp[j + 1].insert((new2 << DIGITS) + new5);
-                if (k == j + 1)
+                int newKey = new2 << DIGITS | new5;
+                if (0 == idxs.count(newKey) || idxs[newKey] > j + 1)
                 {
-                    ans = max(ans, min(new2, new5));
+                    if (0 < idxs.count(newKey))
+                    {
+                        dp[idxs[newKey]].erase(newKey);
+                    }
+
+                    idxs[newKey] = j + 1;
+                    dp[j + 1].insert(newKey);
+                    if (j + 1 + n0 >= k)
+                    {
+                        ans = max(ans, min(new2, new5));
+                    }
                 }
             }
 
             if (0 == j)
             {
                 // Add the current number as a new subset of size 1
-                dp[1].insert((cnt2[i] << DIGITS) + cnt5[i]);
-                if (k == 1)
+                int newKey = (cnt2[i] << DIGITS) + cnt5[i];
+                if (0 == idxs.count(newKey) || idxs[newKey] > j + 1)
                 {
-                    ans = max(ans, min(cnt2[i], cnt5[i]));
+                    if (0 < idxs.count(newKey))
+                    {
+                        dp[idxs[newKey]].erase(newKey);
+                    }
+
+                    idxs[newKey] = j + 1;
+                    dp[j + 1].insert(newKey);
+                    if (j + 1 + n0 >= k)
+                    {
+                        ans = max(ans, min(cnt2[i], cnt5[i]));
+                    }
                 }
             }
         }

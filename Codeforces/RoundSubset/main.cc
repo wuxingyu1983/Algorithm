8d7071908ed3082a896fe913a50fc0308bead472
LeoNg83 <85401996@qq.com>
2026-08-04 17:32:07 +0800

    init

--- Codeforces/RoundSubset/main.cc
+++ Codeforces/RoundSubset/main.cc
@@ -12,13 +12,15 @@
 #include <set>
 #include <queue>
 #include <stack>
-#include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
 const int MAXN = 201;
-
+const int mask = 16383;
+const int DIGITS = 14;
 int cnt2[MAXN], cnt5[MAXN];
+unordered_set<int> dp[MAXN];      // dp[k]
 
 int main()
 {
@@ -46,9 +48,42 @@ int main()
             cnt5[i]++;
             tmp /= 5;
         }
-
-        
     }
+
+    int ans = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (0 == cnt2[i] && 0 == cnt5[i])
+        {
+            continue;
+        }
+
+        for (int j = k - 1; j >= 1; j--)
+        {
+            for (auto it = dp[j].begin(); it != dp[j].end(); it++)
+            {
+                int old2 = *it >> DIGITS;
+                int old5 = *it & mask;
+                int new2 = old2 + cnt2[i];
+                int new5 = old5 + cnt5[i];
+                dp[j + 1].insert((new2 << DIGITS) + new5);
+                if (k == j + 1)
+                {
+                    ans = max(ans, min(new2, new5));
+                }
+            }
+        }
+
+        // Add the current number as a new subset of size 1
+        dp[1].insert((cnt2[i] << DIGITS) + cnt5[i]);
+        if (k == 1)
+        {
+            ans = max(ans, min(cnt2[i], cnt5[i]));
+        }
+    }
+
+    cout << ans << "\n";
 
     return 0;
 }

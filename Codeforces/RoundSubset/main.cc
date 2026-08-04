833a628cddad1ff9fad75e8711fb53410fc92ea7
LeoNg83 <85401996@qq.com>
2026-08-04 16:13:45 +0800

    init

---
+++ Codeforces/RoundSubset/main.cc
@@ -0,0 +1,29 @@
+// https://codeforces.com/contest/837/problem/D
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+
+using namespace std;
+
+const int MAXN = 201;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+
+
+    return 0;
+}

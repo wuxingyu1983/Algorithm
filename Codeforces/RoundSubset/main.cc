d1cfb12f26660586f710bc08d84353f6c1b5be21
LeoNg83 <85401996@qq.com>
2026-08-04 16:20:51 +0800

    init

--- Codeforces/RoundSubset/main.cc
+++ Codeforces/RoundSubset/main.cc
@@ -18,12 +18,37 @@ using namespace std;
 
 const int MAXN = 201;
 
+int cnt2[MAXN], cnt5[MAXN];
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
+    int n, k;
+    cin >> n >> k;
 
+    for (int i = 0; i < n; i++)
+    {
+        long long a;
+        cin >> a;
+
+        long long tmp = a;
+        while (tmp % 2 == 0)
+        {
+            cnt2[i]++;
+            tmp /= 2;
+        }
+        
+        tmp = a;
+        while (tmp % 5 == 0)
+        {
+            cnt5[i]++;
+            tmp /= 5;
+        }
+
+        
+    }
 
     return 0;
 }

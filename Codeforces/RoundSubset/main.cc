dbec2801a17b9f57b7b66796e4f5122d79897a29
LeoNg83 <85401996@qq.com>
2026-08-06 00:40:59 +0800

    AC

--- Codeforces/RoundSubset/main.cc
+++ Codeforces/RoundSubset/main.cc
@@ -23,6 +23,8 @@ const int DIGITS = 14;
 vector<int> cnt2, cnt5;
 unordered_set<int> dp[MAXN];      // dp[k]
 unordered_map<int, int> idxs;
+int offset2[MAXN][12800];           // 个数k的子集，cnt2 - cnt5 = offset，相同 offset 下最大的 cnt2 和 cnt5 的组合
+int offset5[MAXN][12800];
 
 int main()
 {
@@ -74,6 +76,36 @@ int main()
                 int new2 = old2 + cnt2[i];
                 int new5 = old5 + cnt5[i];
                 int newKey = new2 << DIGITS | new5;
+
+                if (new2 - new5 >= 0)
+                {
+                    if (offset2[j + 1][new2 - new5] >= newKey)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        if (0 < offset2[j + 1][new2 - new5])
+                        {
+                            dp[j + 1].erase(offset2[j + 1][new2 - new5]);
+                        }
+                    }
+                }
+                else
+                {
+                    if (offset5[j + 1][new5 - new2] >= newKey)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        if (0 < offset5[j + 1][new5 - new2])
+                        {
+                            dp[j + 1].erase(offset5[j + 1][new5 - new2]);
+                        }
+                    }
+                }
+
                 if (0 == idxs.count(newKey) || idxs[newKey] > j + 1)
                 {
                     if (0 < idxs.count(newKey))
@@ -82,7 +114,19 @@ int main()
                     }
 
                     idxs[newKey] = j + 1;
-                    dp[j + 1].insert(newKey);
+                    if (j + 1 < k)
+                    {
+                        dp[j + 1].insert(newKey);
+
+                        if (new2 - new5 >= 0)
+                        {
+                            offset2[j + 1][new2 - new5] = newKey;
+                        }
+                        else
+                        {
+                            offset5[j + 1][new5 - new2] = newKey;
+                        }
+                    }
                     if (j + 1 + n0 >= k)
                     {
                         ans = max(ans, min(new2, new5));
@@ -102,7 +146,10 @@ int main()
                     }
 
                     idxs[newKey] = j + 1;
-                    dp[j + 1].insert(newKey);
+                    if (j + 1 < k)
+                    {
+                        dp[j + 1].insert(newKey);
+                    }
                     if (j + 1 + n0 >= k)
                     {
                         ans = max(ans, min(cnt2[i], cnt5[i]));

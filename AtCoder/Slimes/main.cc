323283772668b0fa6424739d2c74336883bbfddb
LeoNg83 <85401996@qq.com>
2026-08-09 20:17:14 +0800

    init

---
+++ AtCoder/Slimes/main.cc
@@ -0,0 +1,27 @@
+// https://atcoder.jp/contests/dp/tasks/dp_n
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    
+    return 0;
+}

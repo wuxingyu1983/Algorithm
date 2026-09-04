41246541572dc87dbfc2e07f67f562a2dbe1f2dd
LeoNg83 <85401996@qq.com>
2026-09-05 02:28:28 +0800

    init

--- Codeforces/LookingAtTowers/main.cc
+++ Codeforces/LookingAtTowers/main.cc
@@ -21,6 +21,42 @@ const int MAXN = 300000;
 
 long long pw2[MAXN + 1];
 
+int func(vector<int> &a, vector<long long> &out)
+{
+    int highest = 0;
+
+    vector<int> lis;
+    lis.push_back(a[0]);
+    highest = a[0];
+    for (int i = 1; i < a.size(); i++)
+    {
+        if (highest < a[i])
+        {
+            lis.push_back(a[i]);
+            highest = a[i];
+        }
+    }
+
+    vector<int> pos(lis.size(), -1);
+    vector<int> cnt(lis.size(), 0);
+    vector<long long> dp(lis.size(), 0);
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        int idx = lower_bound(lis.begin(), lis.end(), a[i]) - lis.begin();
+        if (a[i] == lis[idx])
+        {
+
+
+
+            pos[idx] = i;
+            cnt[idx]++;
+        }
+    }
+
+    return highest;
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -63,7 +99,14 @@ int main()
             a[i] = lower_bound(tmp.begin(), tmp.end(), a[i]) - tmp.begin() + 1;
         }
 
-        
+        vector<long long> out(n, 0);
+        int highest = func(a, out);
+
+        reverse(a.begin(), a.end());
+        vector<long long> rout(n, 0);
+        func(a, rout);
+
+
     }
 
     return 0;

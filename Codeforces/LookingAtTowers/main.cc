bb57ada6a816171ada7a763e2e65ca6a3817e175
LeoNg83 <85401996@qq.com>
2026-09-06 03:17:02 +0800

    init

--- Codeforces/LookingAtTowers/main.cc
+++ Codeforces/LookingAtTowers/main.cc
@@ -13,6 +13,7 @@
 #include <set>
 #include <queue>
 #include <stack>
+#include <unordered_map>
 
 using namespace std;
 
@@ -20,6 +21,94 @@ const long long MOD = 998244353;
 const int MAXN = 300000;
 
 long long pw2[MAXN + 1];
+
+
+template <typename T>
+class SegmentTree {
+private:
+    int n;
+    std::vector<T> tree;
+    std::vector<T> lazy;
+
+    // Helper function to push pending updates down to children
+    void push(int node, int start, int end)
+    {
+        if (lazy[node] != 0)
+        {
+            int mid = start + (end - start) / 2;
+
+            // Update left child
+            tree[2 * node] += (mid - start + 1) * lazy[node];
+            lazy[2 * node] += lazy[node];
+
+            // Update right child
+            tree[2 * node + 1] += (end - mid) * lazy[node];
+            lazy[2 * node + 1] += lazy[node];
+
+            // Clear current node's lazy value
+            lazy[node] = 0;
+        }
+    }
+
+    void update_range(int node, int start, int end, int ql, int qr, T val)
+    {
+        // No overlap
+        if (qr < start || ql > end)
+            return;
+
+        // Total overlap
+        if (ql <= start && end <= qr)
+        {
+            tree[node] += (end - start + 1) * val;
+            if (start != end)
+            { // Not a leaf
+                lazy[2 * node] += val;
+                lazy[2 * node + 1] += val;
+            }
+            return;
+        }
+
+        // Partial overlap: push down, update children, and recalculate parent
+        push(node, start, end);
+        int mid = start + (end - start) / 2;
+        update_range(2 * node, start, mid, ql, qr, val);
+        update_range(2 * node + 1, mid + 1, end, ql, qr, val);
+        tree[node] = tree[2 * node] + tree[2 * node + 1];
+    }
+
+    T query_range(int node, int start, int end, int ql, int qr)
+    {
+        // No overlap
+        if (qr < start || ql > end)
+            return 0;
+
+        // Total overlap
+        if (ql <= start && end <= qr)
+            return tree[node];
+
+        // Partial overlap: push down updates before reading children
+        push(node, start, end);
+        int mid = start + (end - start) / 2;
+        T leftSum = query_range(2 * node, start, mid, ql, qr);
+        T rightSum = query_range(2 * node + 1, mid + 1, end, ql, qr);
+        return leftSum + rightSum;
+    }
+
+public:
+    SegmentTree(int size) {
+        n = size;
+        tree.assign(4 * n, 0);
+        lazy.assign(4 * n, 0);
+    }
+
+    void update(int l, int r, T val) {
+        update_range(1, 0, n - 1, l, r, val);
+    }
+
+    T query(int l, int r) {
+        return query_range(1, 0, n - 1, l, r);
+    }
+};
 
 int func(vector<int> &a, vector<long long> &out)
 {
@@ -37,20 +126,23 @@ int func(vector<int> &a, vector<long long> &out)
         }
     }
 
-    vector<int> pos(lis.size(), -1);
-    vector<int> cnt(lis.size(), 0);
-    vector<long long> dp(lis.size(), 0);
+    vector<long long> dp(a.size(), 0);          // dp[i] 在 lis 中第一次出现的数目
+    unordered_map<int, int> pos;                // lis 中的某一位在 lis 中出现的位置
+    vector<int> preCnt(a.size(), 0);            // preCnt[i] 表示 a[0 - i] 之间小于等于a[i]的个数
+    SegmentTree<int> segTree(highest + 1);      // 线段树，用于计算 preCnt
 
     for (int i = 0; i < a.size(); i++)
     {
         int idx = lower_bound(lis.begin(), lis.end(), a[i]) - lis.begin();
         if (a[i] == lis[idx])
         {
+            // a[i] 元素在 lis 中出现
 
 
+        }
+        else
+        {
 
-            pos[idx] = i;
-            cnt[idx]++;
         }
     }
 

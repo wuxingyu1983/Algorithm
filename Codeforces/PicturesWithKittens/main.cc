510f4049a89c8b28a4c8e569e4f7151315894316
LeoNg83 <85401996@qq.com>
2026-09-10 02:19:11 +0800

    init

---
+++ Codeforces/PicturesWithKittens/main.cc
@@ -0,0 +1,36 @@
+// https://codeforces.com/contest/1077/problem/F1
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, k, x;
+    cin >> n >> k >> x;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    
+
+    return 0;
+}

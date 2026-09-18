ad07b307393bc649f99841c435b14308a3223bc5
LeoNg83 <85401996@qq.com>
2026-09-18 22:39:51 +0800

    init

---
+++ Codeforces/MoneyBuysHappiness/main.cc
@@ -0,0 +1,37 @@
+// https://codeforces.com/problemset/problem/1974/E
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <deque>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    for (int iT = 0; iT < t; iT++)
+    {
+        int m, x;
+        cin >> m >> x;
+
+        
+    }
+
+    return 0;
+}

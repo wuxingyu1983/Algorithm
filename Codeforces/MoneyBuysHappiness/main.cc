470c358e166f45505861a764b3d277351c9f1922
LeoNg83 <85401996@qq.com>
2026-09-19 00:48:01 +0800

    AC

--- Codeforces/MoneyBuysHappiness/main.cc
+++ Codeforces/MoneyBuysHappiness/main.cc
@@ -17,6 +17,10 @@
 
 using namespace std;
 
+int cs[50], hs[50];
+
+long long dp[100001];
+
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -25,12 +29,71 @@ int main()
     int t;
     cin >> t;
 
-    for (int iT = 0; iT < t; iT++)
+    for (int i = 0; i < t; i++)
     {
         int m, x;
         cin >> m >> x;
 
-        
+        int sum = 0;
+        for (int j = 0; j < m; j++)
+        {
+            cin >> cs[j] >> hs[j];
+
+            sum += hs[j];
+        }
+
+        memset(dp, 0, sizeof(dp));
+
+        int ans = 0;
+
+        // month 0
+        {
+            if (0 == cs[0])
+            {
+                dp[hs[0]] = x;
+
+                if (1 == m)
+                {
+                    ans = hs[0];
+                }
+            }
+            else
+            {
+                dp[0] = x;
+            }
+        }
+
+        for (int j = 1; j < m; j++)
+        {
+            for (int k = sum; k >= 0; k--)
+            {
+                if (0 < dp[k])
+                {
+                    // 消费，获取快乐
+                    if (dp[k] >= cs[j])
+                    {
+                        dp[k + hs[j]] = max(dp[k + hs[j]], dp[k] - cs[j] + x);
+
+                        if (m - 1 == j)
+                        {
+                            ans = max(ans, k + hs[j]);
+                        }
+                    }
+
+                    // 不消费
+                    {
+                        dp[k] += x;
+
+                        if (m - 1 == j)
+                        {
+                            ans = max(ans, k);
+                        }
+                    }
+                }
+            }
+        }
+
+        cout << ans << "\n";
     }
 
     return 0;

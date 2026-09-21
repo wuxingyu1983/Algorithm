1aaff7764ce5bd36a79ae970a55155124b95609b
LeoNg83 <85401996@qq.com>
2026-09-22 02:02:09 +0800

    init

---
+++ Codeforces/MaximumColorSegment/main.cc
@@ -0,0 +1,28 @@
+// https://codeforces.com/problemset/problem/2172/L
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <deque>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    
+
+    return 0;
+}
